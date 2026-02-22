0beed0bbafd712e17859b9e626d432d59422f4ae
mune001 <edmune25@gmail.com>
2026-02-22 16:35:17 +0300

    cleaned up unnecessary imports. Ready to Push

--- common/views.py
+++ common/views.py
@@ -1,13 +1,15 @@
-from typing import override, Any
+from typing import override
 from rest_framework.views import APIView
-from ipware import get_client_ip
 import structlog
+from django.contrib.auth.models import AbstractUser
+from contextvars import ContextVar
 
 class BaseAPIView(APIView):
     @override
     def perform_authentication(self, request):
         super().perform_authentication(request)
         structlog.contextvars.bind_contextvars(
-                user_id=request.user.pk if request.user.is_authenticated else None,
-                ip=get_client_ip(request),
-            )
+            user_id= request.user.pk if  request.user.is_authenticated else None,
+        )
+
+
