b1621ebb4693cc5dba94a889c9d546b1ae40173b
mune001 <edmune25@gmail.com>
2026-03-05 16:40:53 +0300

    Testing deactivation and unsubs done

--- users/services/deactivate.py
+++ users/services/deactivate.py
@@ -8,23 +8,36 @@ logger = getLogger("users.services.unsub")
 def account_unsubscribe(account: Account) -> Account:
     """
     Unsubscribes a user from the mailing list.
+
+    :param account: account obj
+    :type accoutn: Account
+
+    :return: unsubed account instance
+    :rtype: Account
     """
     if account.can_receive_emails:
         account.can_receive_emails = False
-        account.save()
+        account.save(update_fields=["can_receive_emails"])
         logger.info(f"user [user_id: {account.user_id}] unsubscribed from mailling list")  # type: ignore
     return account
 
 
 
 @transaction.atomic
-def user_deactivate(user: User) -> User | None:
+def user_deactivate(user: User) -> User:
     """
-    Allows deletion or deactivation of account.
-    Denies permission an actor(Administrator) deleting or deactivating a super_user or staff member
-    """     
-    user.is_active = False
-    user.save()
-    logger.info(f"user [user_id: {user.pk}] deactivated.")
+    Simplified the delete/deactivate dance: No deletions apart from the admin.
+    This service serves both the user and admin deactiavations
+
+    :param user: The user obj
+    :type user: User
+
+    :return: deactivated user
+    :rtype: User
+    """ 
+    if user.is_active:
+        user.is_active = False
+        user.save(update_fields=["is_active"])
+        logger.info(f"user [user_id: {user.pk}] deactivated.")
     return user
     
---
+++ users/tests/test_deactivate_services.py
@@ -0,0 +1,38 @@
+import pytest
+from datetime import timedelta
+from django.utils import timezone
+from rest_framework.exceptions import ValidationError
+from users.models import User
+from users.services import user_deactivate, account_unsubscribe
+
+class TestUserDeactivation:
+    def test_deactivation_succeeds(self, user: User, django_assert_num_queries):
+        """
+        Very simple, just check whether the returned user is deactivated
+        Also check the number of querries
+        """
+        with django_assert_num_queries(3):
+            mod_user = user_deactivate(user)
+        fetched = User.objects.get(pk=user.pk)
+        assert fetched == mod_user == user
+        assert mod_user.is_active == False
+
+        with django_assert_num_queries(2):
+            user_deactivate(mod_user)
+        
+class TestAccountUnsubscribe:
+    def test_deactivation_succeeds(self, user: User, django_assert_num_queries):
+        """
+        Account returned should not allow emails
+        Count querries aggree
+        """
+        with django_assert_num_queries(3):
+            mod_acc = account_unsubscribe(user.account)
+        fetched = User.objects.get(pk=user.pk).account
+        assert fetched == mod_acc == user.account
+        assert mod_acc.can_receive_emails == False
+
+        with django_assert_num_queries(2):
+            account_unsubscribe(mod_acc)
+
+        
