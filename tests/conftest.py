5ed7d9365c25bd45210c1d198ada85311a2bb849
mune001 <edmune25@gmail.com>
2026-03-12 12:00:30 +0300

    Added cache clearing to conftest. Required for clearing cache in between tests esp throttle classes.
    Crazy how the client class was using the same instance of the client making each seperate client  authenticated as the last used client.
    Also the user instance was using the same user instance and using multiple users compounds the permissions on the same user instance

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from phonenumber_field.phonenumber import PhoneNumber
 from users.models import User
 from users.services import user_account_create
 from rest_framework.test import APIClient
-
+from django.core.cache import cache
 
 @pytest.fixture(scope="session")
 def django_db_setup(django_db_setup, django_db_blocker):
@@ -21,9 +21,14 @@ def enable_db_access(db):
     """Automatically enable db access for all tests."""
     pass
 
+@pytest.fixture(scope="function")
+def cache_clear():
+    yield
+    cache.clear()
+
 
 @pytest.fixture(autouse=True)
-def globals(settings):
+def settings_override(settings):
     settings.CELERY_TASK_ALWAYS_EAGER = True
     settings.CELERY_TASK_EAGER_PROPAGATES = True
     settings.EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
@@ -33,7 +38,6 @@ def globals(settings):
             "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
         }
     }
-
 
 
 @pytest.fixture
@@ -46,19 +50,31 @@ def fake():
 
 
 @pytest.fixture
-def user(fake, phone_no) -> User:
+def user_factory(fake, phone_no) -> typing.Callable[[int, dict[str, typing.Any]], list[User]]:
     """
     Returns a user object with an account created
     """
-    
-    return user_account_create(
-        first_name=fake.first_name(),
-        last_name=fake.last_name(),
-        password="Pa55word!",
-        email=fake.email(),
-        phone_number=phone_no(),
-        notify=False,
-    )
+
+    def create(quantity: int = 1, overrides: dict[str, typing.Any] = {}) -> list[User]:
+        users = []
+        for _ in range(quantity):
+            users.append(
+                user_account_create(
+                    first_name=(overrides.get("fist_name", fake.first_name())),
+                    last_name=overrides.get("last_name", fake.last_name()),
+                    password="Pa55word!",
+                    email=overrides.get("email", fake.email()),
+                    phone_number=overrides.get("phone_number", phone_no()),
+                    notify=False,
+                )
+            )
+        return users
+
+    return create
+
+@pytest.fixture
+def user(user_factory) -> User:
+    return user_factory()[0]
 
 @pytest.fixture
 def roles_list() -> list[Group]:
@@ -72,30 +88,34 @@ def roles_list() -> list[Group]:
 def get_role() -> typing.Callable[[str], Group]:
     """
     Callable to return a specific group
-    Pass in the str value of the group 
+    Pass in the str value of the group
     """
     return lambda name: Group.objects.get(name=name)
 
 
 @pytest.fixture
-def manager_user(user, get_role) -> User:
-    user.groups.add(get_role("manager"))
-    return user
+def manager_user(user_factory, get_role) -> User:
+    u = user_factory()[0]
+    u.groups.add(get_role("manager"))
+    return u
 
 
 @pytest.fixture
-def caretaker_user(user, get_role) -> User:
-    user.groups.add(get_role("caretaker"))
-    return user
+def caretaker_user(user_factory, get_role) -> User:
+    u = user_factory()[0]
+    u.groups.add(get_role("caretaker"))
+    return u
 
 
 @pytest.fixture
-def tenant_user(user, get_role) -> User:
-    user.groups.add(get_role("tenant"))
-    return user
+def tenant_user(user_factory, get_role) -> User:
+    u = user_factory()[0]
+    u.groups.add(get_role("tenant"))
+    return u
+
 
 @pytest.fixture
-def password():
+def password() -> str:
     return "Pa55word!"
 
 
@@ -105,32 +125,46 @@ def phone_no(fake) -> typing.Callable[[str | None], PhoneNumber]:
     A callable to generate a phone number object
     """
 
-    return lambda num=None: PhoneNumber.from_string(
-        num or fake.numerify("+254-7##-###-###")
-    )
+    return lambda num=None: PhoneNumber.from_string(num or fake.numerify("+254-7##-###-###"))
 
 
 @pytest.fixture
-def client():
+def client() -> APIClient:
     return APIClient()
 
-@pytest.fixture
-def auth_manager(client, manager_user):
-    client.force_authenticate(user=manager_user)
-    return client
 
 @pytest.fixture
-def auth_user(client, user):
+def user_client(user) -> APIClient:
+    client = APIClient()
     client.force_authenticate(user=user)
     return client
 
+
 @pytest.fixture
-def auth_tenant(client, tenant_user):
+def manager_client(manager_user) -> APIClient:
+    client = APIClient()
+    client.force_authenticate(user=manager_user)
+    return client
+
+
+@pytest.fixture
+def tenant_client(tenant_user) -> APIClient:
+    client = APIClient()
     client.force_authenticate(user=tenant_user)
     return client
 
+
 @pytest.fixture
-def auth_caretaker(client, caretaker_user):
+def caretaker_client(caretaker_user) -> APIClient:
+    client = APIClient()
     client.force_authenticate(user=caretaker_user)
     return client
 
+
+@pytest.fixture
+def super_user_client(user) -> APIClient:
+    client = APIClient()
+    user.is_superuser = True
+    user.save(update_fields=["is_superuser"])
+    client.force_authenticate(user=user)
+    return client
