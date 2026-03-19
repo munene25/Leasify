310834a0fcc205763c1b58ada2bd8a602a080446
mune001 <edmune25@gmail.com>
2026-03-19 12:27:44 +0300

    Renamed login folder to authenticate

--- users/services/__init__.py
+++ users/services/__init__.py
@@ -2,5 +2,5 @@ from .create import user_account_create, account_create, user_add_roles
 from .update import  user_change_password, user_update
 from .deactivate import user_deactivate, account_unsubscribe, user_remove_roles
 from .deactivate import account_unsubscribe, user_deactivate
-from .login import user_authenticate
+from .authenticate import user_authenticate
 from .emails import user_email_verify, user_email_update
