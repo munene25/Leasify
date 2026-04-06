1222776a0801fecdf072055ff14261b8112c3a32
munene25 <edmune25@gmail.com>
2026-04-06 15:26:26 +0300

    common.exceptions - Added a Role Assignment Error for when a user is trying to be associated with more than one group. - Renamed the Exceptions to follow best practices with "Error"

--- common/exceptions.py
+++ common/exceptions.py
@@ -6,14 +6,26 @@ class EmailUpdateError(APIException):
     """Raised when email updates are attempted before email cooldown elapses"""
 
     status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
-    default_code = 'email_cooldown_active'
+    default_code = "email_cooldown_active"
 
 
-class UserLinkMalformed(APIException):
+class UserLinkMalformedError(APIException):
     """
     Raised when uidb64 or token segments in a generated URL 
     cannot be decoded or are structurally invalid.
     """
+
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = "The provided link is malformed or has expired."
     default_code = "link_malformed"
+
+
+class RoleAssignmentError(APIException):
+    """
+    Raised when uidb64 or token segments in a generated URL
+    cannot be decoded or are structurally invalid.
+    """
+
+    status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+    default_detail = "Only one role is allowed per user."
+    default_code = "roles_exceeded"
