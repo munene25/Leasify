10d88d4d239d62552737b16a539286d5db2f65ce
munene25 <edmune25@gmail.com>
2026-04-07 14:50:19 +0300

    users.services: - user_set_role now takes a replace argument to force caller to acknowledge its a role may exist and intends to override. - renamed user_add_roles to user_set_role - reformated file

--- users/services/create.py
+++ users/services/create.py
@@ -8,8 +8,11 @@ from users.tasks import send_welcome_email
 
 logger = getLogger("users.services.create")
 
+
 @transaction.atomic
-def user_account_create(*, password: str, email: str, phone_number: PhoneNumber, first_name:str, last_name: str , notify: bool) -> User:
+def user_account_create(
+    *, password: str, email: str, phone_number: PhoneNumber, first_name: str, last_name: str, notify: bool
+) -> User:
     email = User.objects.normalize_email(email)
     user = User(
         email=email,
@@ -24,15 +27,18 @@ def user_account_create(*, password: str, email: str, phone_number: PhoneNumber,
 
     if notify == True:
         transaction.on_commit(lambda: send_welcome_email.delay(user.pk))
-    
+
     logger.info(f"user {user.get_full_name()} [user_id: {user.pk}] created an account")
     return user
 
+
 @transaction.atomic
-def account_create(*, user: User, phone_number: PhoneNumber, bio: str | None = None, backup_email: str | None = None ) -> Account:
+def account_create(
+    *, user: User, phone_number: PhoneNumber, bio: str | None = None, backup_email: str | None = None
+) -> Account:
     """
     Creates an account instance linked to a user one-one-field
-    
+
     :param user: User to be used as the related field
     :type user: User
     :param phone_number: Phone number (region KE) for payment processing
@@ -44,30 +50,33 @@ def account_create(*, user: User, phone_number: PhoneNumber, bio: str | None = N
     :return: An account object
     :rtype: Account
     """
-    
-    backup_email = User.objects.normalize_email(backup_email) 
-    account = Account(
-        user=user, phone_number=phone_number, bio=bio, backup_email=backup_email
-    )
+
+    backup_email = User.objects.normalize_email(backup_email)
+    account = Account(user=user, phone_number=phone_number, bio=bio, backup_email=backup_email)
     account.full_clean()
     account.save()
     return account
 
+
 @transaction.atomic
-def user_add_role(*, user: User, role: Group)-> User:
+def user_set_role(*, user: User, role: Group, replace: bool = False) -> User:
     """
-        Responsible for adding a user to a group.
-        Ensures that only one role can be assigned to a user at a time.
-        
-        :param user: The user to whom the role will be added.
-        :type user: User
-        :param role: The role (Group) to be added to the user.
-        :type role: Group
-        :return: The user with the newly added role.
-        :rtype: User    
+    Responsible for adding a user to a group.
+    Ensures that only one role can be assigned to a user at a time.
+    replace flag explicitly requires the caller to acknowledge that an existing role will be replaced if it exists.
+
+    :param user: The user to whom the role will be added.
+    :type user: User
+    :param role: The role (Group) to be added to the user.
+    :type role: Group
+    :param replace: If True, allows replacing an existing role.
+    :type replace: bool
+    :raises RoleAssignmentError: If the user already has a role and replace is False.
+    :return: The user with the newly added role.
+    :rtype: User
     """
-    if user.groups.exists():
+    if user.groups.exists() and replace == False:
         raise RoleAssignmentError()
-    user.groups.add(role)
+    user.groups.set([role])
     logger.info(f"Roles [role: {role}] added for [user_id: {user.pk}]")
     return user
