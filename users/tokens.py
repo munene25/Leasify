9eac5dc1506a92f06ad238efa12ab48753634ef3
munene25 <edmune25@gmail.com>
2026-04-07 12:19:39 +0300

    users.tokens: - UserLinkMalformedError was renamed

--- users/tokens.py
+++ users/tokens.py
@@ -3,11 +3,12 @@ from django.utils.encoding import force_bytes, force_str
 from django.contrib.auth.tokens import default_token_generator
 from rest_framework.exceptions import NotFound
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from common.exceptions import UserLinkMalformed
+from common.exceptions import UserLinkMalformedError
 from users.models import User
 from users.selectors import user_get
 
 BASE_URL: str = getattr(settings, "FRONTEND_URL")
+
 
 def build_user_url(*, user: User, path: str, with_token: bool = True) -> str:
     """
@@ -18,21 +19,24 @@ def build_user_url(*, user: User, path: str, with_token: bool = True) -> str:
     if with_token:
         segments.append(token_generate(user))
 
-    return  "/".join(seg for seg in segments)
+    return "/".join(segments)
 
 
-def token_generate(user: User)-> str:
+def token_generate(user: User) -> str:
     """create a one time use token for a user"""
     return default_token_generator.make_token(user)
+
 
 def token_validate(*, user: User, token: str) -> None:
     """Check provided token for expiry and validity"""
     if not default_token_generator.check_token(user, token):
-        raise UserLinkMalformed() 
+        raise UserLinkMalformedError()
+
 
 def uidb64_generate(user: User) -> str:
     """Returns the user_id as a base 64 encoded string"""
     return urlsafe_base64_encode(force_bytes(user.pk))
+
 
 def get_user_from_uidb64(uidb64: str) -> User:
     """Decode base 64 encoded string and get the associated user"""
@@ -40,4 +44,4 @@ def get_user_from_uidb64(uidb64: str) -> User:
         user_id = force_str(urlsafe_base64_decode(uidb64))
         return user_get(int(user_id))
     except (ValueError, NotFound):
-        raise UserLinkMalformed()
+        raise UserLinkMalformedError()
