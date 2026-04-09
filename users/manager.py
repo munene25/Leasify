d99f2da42d076d99857b2442ca6820ec49002732
munene25 <edmune25@gmail.com>
2026-04-09 16:21:03 +0300

    users.manager: - created a user manager for create user hooking into the user service layers.

---
+++ users/manager.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email: str, password: str | None = None, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        from users.services import user_account_create
+
+        if not password:
+            raise ValueError("Password must be provided")
+        
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('first_name', "anon")
+        extra_fields.setdefault('last_name', "anon")
+
+        user = user_account_create(
+            email=email,
+            password=password,
+            first_name=extra_fields['first_name'],
+            last_name=extra_fields['first_name'],
+            notify=True,
+        )
+        return user
+    
+    def create_superuser(self, email: str, password: str | None = None, **extra_fields):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+            email=email,
+            password=password,
+            **extra_fields,
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.verified = True
+        user.save()
+        return user
+
+
+
+
