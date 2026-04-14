f69f54cc4078d56302326d87746b9e5d76fb2906
munene25 <edmune25@gmail.com>
2026-04-14 18:22:15 +0300

    users.services.__init__: - added user_create

--- users/services/__init__.py
+++ users/services/__init__.py
@@ -1,4 +1,4 @@
-from .create import user_account_create, account_create, user_set_role
+from .create import user_account_create, account_create, user_set_role, user_create
 from .update import user_change_password, user_update
 from .deactivate import user_change_active_status, account_unsubscribe, user_remove_role
 from .authenticate import user_authenticate
