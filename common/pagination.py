8bed7e588f6ae1eb1ee28a96415ba82c435022ef
munene25 <edmune25@gmail.com>
2026-04-16 15:56:24 +0300

    common.pagination: - changed get_paginated_response call signature to better named arguments

--- common/pagination.py
+++ common/pagination.py
@@ -1,15 +1,20 @@
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.serializers import Serializer
+from django.db.models import QuerySet
+from rest_framework.request import Request
+from rest_framework.views import APIView
 
-def get_paginated_response(*, s_cls, qs, req, view):
+def get_paginated_response(*, serializer_class: type[Serializer], queryset: QuerySet, request: Request, view: APIView) -> Response:
     """Return paginated response for list views"""
+
     paginator = PageNumberPagination()
-    page = paginator.paginate_queryset(qs, req, view=view)
+    page = paginator.paginate_queryset(queryset, request, view=view)
 
     if page is not None:
-        serializer = s_cls(page, many=True)
+        serializer = serializer_class(page, many=True)
         return paginator.get_paginated_response(serializer.data)
 
-    serializer = s_cls(qs, many=True)
+    serializer = serializer_class(queryset, many=True)
 
     return Response(data=serializer.data, status=200)
