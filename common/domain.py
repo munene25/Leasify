85ca3798968be1806f8b533a401154dc677471b3
munene25 <edmune25@gmail.com>
2026-04-16 15:54:43 +0300

    common.domain: - this is essentially a domain config for exclusions per model

---
+++ common/domain.py
@@ -0,0 +1,25 @@
+from users.models import User
+from django.db.models import Q
+
+
+class RoleBasedExclusions:
+    """
+    A tiered mapping of how each role affects how list views are rendered based on the user's role
+    """
+
+    SUPERUSER = Q()
+    MANAGER = Q()
+    CARETAKER = Q()
+    TENANT = Q()
+    GENERAL = Q()
+
+    @classmethod
+    def for_user(cls, user: User):
+        base_exclusion = {
+            "superuser": cls.SUPERUSER,
+            "manager": cls.MANAGER,
+            "caretaker": cls.CARETAKER,
+            "tenant": cls.TENANT,
+            "general": cls.GENERAL,
+        }[user.role]
+        return base_exclusion
