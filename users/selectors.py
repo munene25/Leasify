aa1eb994a96d0f370af92ca3fab08b9de0c4e20c
munene25 <edmune25@gmail.com>
2026-04-17 09:56:47 +0300

    users.selectors: - Role based exlusions is now a parent class which defines the class method. - removed stale imports. - raise_not_found changed to user_not_found. - user_list was not called anywhere hence was removed.

--- users/selectors.py
+++ users/selectors.py
@@ -1,42 +1,27 @@
 from typing import Any
-import django_filters
+
 from django.db.models import Q
 from django.http import QueryDict
 from django.contrib.auth.models import UserManager
 from django.db.models.query import QuerySet
 from django.contrib.auth.models import Group
-from rest_framework.exceptions import NotFound
 from users.models import User
-from common.helpers import not_found
+from common.helpers import raise_not_found
+from common.domain import RoleBasedExclusions
+
+# base queryset with related account to avoid repetition of select_related in each selector
+BASE_QS = User.objects.select_related("account")
+
+# Initialize the raise_not_found wrapper for most common exception
+user_not_found = raise_not_found("user_id", "User with given id not found")
 
 
-class UserRoleBasedExclusion:
-    """
-    A tiered mapping of how each role affects which users can be viewed by the role
-    ! It also excludes the current user from the queryset
-    """
-
+class UserExclusions(RoleBasedExclusions):
     SUPERUSER = Q()
     MANAGER = Q(is_superuser=True)
     CARETAKER = MANAGER | Q(groups__name="manager")
-    GENERAL = CARETAKER | Q(groups__name="caretaker")
-
-    @classmethod
-    def for_user(cls, user: User):
-        base_exclusion = {
-            "superuser": cls.SUPERUSER,
-            "manager": cls.MANAGER,
-            "caretaker": cls.CARETAKER,
-            "general": cls.GENERAL,
-        }[user.role]
-
-        return base_exclusion | Q(pk=user.pk)
-
-
-BASE_QS = User.objects.select_related("account")
-
-# Initialize the not_found wrapper for most common exception
-raise_not_found = not_found("user_id", "User with given id not found")
+    TENANT = CARETAKER | Q(groups__name="caretaker")
+    GENERAL = TENANT
 
 
 def user_list_for(*, user: User, filters: dict[str, Any] | QueryDict | None = None) -> QuerySet:
@@ -45,6 +30,7 @@ def user_list_for(*, user: User, filters: dict[str, Any] | QueryDict | None = No
     Allows filtering based on fileds "search" and "is_active"
     search includes: first_name, last_name, email and account__phone_number
     """
+    import django_filters
 
     class UserFilter(django_filters.FilterSet):
         search = django_filters.CharFilter(method="search_fields")
@@ -54,27 +40,20 @@ def user_list_for(*, user: User, filters: dict[str, Any] | QueryDict | None = No
             fields = ("is_active",)
 
         def search_fields(self, queryset, name, value):
-            qs = queryset.filter(
+            return queryset.filter(
                 Q(first_name__icontains=value)
                 | Q(last_name__icontains=value)
                 | Q(email__icontains=value)
                 | Q(account__phone_number__contains=value)
             )
-            return qs
 
-    exclusions = UserRoleBasedExclusion.for_user(user)
-    users = BASE_QS.exclude(exclusions)
+    exclusions = UserExclusions.for_user(user)
+    users = BASE_QS.exclude(exclusions, user_id=user.pk)
     return UserFilter(filters, users).qs
 
 
-def user_list():
-    """
-    Get a full list of all the users in the db.
-    """
-    return BASE_QS.all()
 
-
-@raise_not_found
+@user_not_found
 def user_get(user_id: int) -> User:
     """
     The primary way to fetch data for non admin routes and within domain in general
@@ -82,20 +61,18 @@ def user_get(user_id: int) -> User:
     return BASE_QS.get(pk=user_id)
 
 
+@user_not_found
 def user_get_for(*, user: User, user_id: int) -> User:
     """
     This is primarily for admin routes to exclude certain users from the queryset
     User will *only* be able to view users intended to be visible to them.
     """
 
-    exclusions = UserRoleBasedExclusion.for_user(user)
-    matched_user = BASE_QS.filter(pk=user_id).exclude(exclusions).first()
-    if matched_user is None:
-        raise NotFound({"user_id": "user not found"})
-    return matched_user
+    exclusions = UserExclusions.for_user(user)
+    return BASE_QS.exclude(exclusions).get(user_id=user_id)
 
 
-@not_found("email", "User with given email not found")
+@raise_not_found("email", "User with given email not found")
 def user_get_by_email(user_email) -> User:
     """
     Used in views where the email is the only identifying attribute eg. password-reset
@@ -106,7 +83,7 @@ def user_get_by_email(user_email) -> User:
     return BASE_QS.get(email=email)
 
 
-@raise_not_found
+@user_not_found
 def user_get_locked(user_id: int) -> User:
     """
     Necessary for locking row access while updating
@@ -120,4 +97,3 @@ def groups_list():
     Exists as a User selector as it's highly coupled with the user model
     """
     return Group.objects.all()
- 
