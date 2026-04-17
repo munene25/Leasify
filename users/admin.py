31d1500aa96fa383de04440d2ca34f9318ef4dc2
munene25 <edmune25@gmail.com>
2026-04-17 11:12:11 +0300

    users.admin: - fixed the user admin panel, now groups can only be assigned one at a time through a choice field

--- users/admin.py
+++ users/admin.py
@@ -7,6 +7,7 @@ from .models import Account
 from django import forms
 from structlog import get_logger
 from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import Group
 
 
 logger = get_logger("users.admin")
@@ -23,6 +24,24 @@ class AccountAdminForm(forms.ModelForm):
         return value
 
 class UserAdminForm(forms.ModelForm):
+    group = forms.ChoiceField(choices=[], help_text="Select a group to assign to the user to.", label="Group")
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        group_field = self.fields["group"]
+        group_choices = [("none", "None")]
+        initial = "none"
+        
+        for group in Group.objects.all():
+            if self.instance and self.instance.groups.first() == group:
+                initial = group.name
+            group_choices.append((group.name, group.name.capitalize()))
+
+        group_field.choices = group_choices
+        group_field.initial = initial
+
+
     class Meta:
         model = User
         fields = "__all__"
@@ -52,7 +71,7 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ("email", "full_name", "is_staff", "is_superuser", "is_active", "phone_number")
     list_select_related = ("account",)
     form = UserAdminForm
-    exclude = ("last_login", "created_at", "last_email_change")
+    exclude = ("last_login", "created_at", "last_email_change", "groups")
     search_fields = ("username", "email", "first_name", "last_name")
     list_filter = ("is_active",)
     can_delete = False
@@ -68,19 +87,30 @@ class UserAdmin(admin.ModelAdmin):
         Hook for intercepting the save action for the user model.
         Used to hash the password when creating a user via the admin panel and to log the action.
         """
+        
+            
         if "password" in form.changed_data:
             obj.set_password(form.cleaned_data["password"])
+
         state = {True: "updated", False: "created"}[change]
         logger.info(f"admin {state} user {obj.get_full_name()} [user_id: {obj.pk}]. data[{form.changed_data}]")
         super().save_model(request, obj, form, change)
-
+        
     def save_related(self, request, form, formsets, change):
         """
         Hook for intercepting the save action for the related user models.
         Includes account formset. Used to log the action.
         """
+        if "group" in form.changed_data:
+            from users.services import user_set_role, user_remove_role
+            user, role = form.instance, form.cleaned_data.get("group")
+            if role is "none":
+                user_remove_role(user)
+            else: 
+                group = Group.objects.get(name=role)
+                user_set_role(user=user, role=group, replace=True)
+
+        state = {True: "updated", False: "created"}[change]
         for formset in formsets:
-            state = {True: "updated", False: "created"}[change]
-            if formset.model == Account and formset.forms and formset.forms[0].has_changed():
-                logger.info(f"admin {state} account for user [user_id: {form.instance.pk}]. data[{formset.forms[0].changed_data}]")
+            logger.info(f"admin {state} {formset.model} for user [user_id: {form.instance.pk}]")
         super().save_related(request, form, formsets, change)
