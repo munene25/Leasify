3848cc5034b49e500520cf33069f4340b2aae74a
munene25 <edmune25@gmail.com>
2026-04-20 15:54:29 +0300

    apartments.selectors: - removed stale imports - added a get_base_qs function - added a try except block when type coercing just to be safe in apartment_list_for() - improved docstrings

--- apartments/selectors.py
+++ apartments/selectors.py
@@ -1,16 +1,15 @@
 from typing import Any
 from django.db import models
-from rest_framework.exceptions import NotFound
 from apartments.models import Apartment
 from semesters.models import Semester
+
 from common.helpers import raise_not_found
 from users.models import User
 from common.domain import RoleBasedExclusions
 
+
 # wrapper to raise not found for each id
 apartment_not_found = raise_not_found("apartment_id", "Apartment does not exist")
-
-BASE_PREFETCH_QS = Apartment.objects.prefetch_related("tenancy_set")
 
 
 class ApartmentExclusions(RoleBasedExclusions):
@@ -23,13 +22,30 @@ class ApartmentExclusions(RoleBasedExclusions):
     GENERAL = TENANT
 
 
+def get_base_qs_with_current_tenant_prefetch() -> models.QuerySet[Apartment]:
+    """
+    This is defined within a function to avoid prematurely evaluating the current_semester
+    """
+
+    from semesters.selectors import semester_current
+    from tenancy.models import Tenancy
+
+    sem = semester_current()
+
+    qs = Apartment.objects.prefetch_related(
+        models.Prefetch("tenancy_set", Tenancy.objects.select_related("user").filter(semester_id=sem.pk))
+    )
+    return qs
+
 def apartment_list_for(*, user: User, filters: dict[str, Any] | None = None):
     """
     Fetches apartment list visible for the requesting user.
     filters based on fields: search and rentable.
+    rent can be filtered against rent_max and rent_min as well
+    prefetch is necessary in the list view to be able to tell if it's occupied.
     """
+
     import django_filters
-    from django.db.models.functions import Cast
     from decimal import Decimal
 
     class ApartmentFilter(django_filters.FilterSet):
@@ -43,20 +59,23 @@ def apartment_list_for(*, user: User, filters: dict[str, Any] | None = None):
         def search_fields(self, queryset, name, value):
             query = models.Q(block__icontains=value)
             if value.isdigit():
-                query |= models.Q(unit_number=int(value)) | models.Q(rent=Decimal(value))
+                try:
+                    query |= models.Q(unit_number=int(value)) | models.Q(rent=Decimal(value))
+                except ValueError:
+                    pass
             return queryset.filter(query)
 
     exclusions = ApartmentExclusions.for_user(user)
-    apartments = BASE_PREFETCH_QS.exclude(exclusions)
+    apartments = get_base_qs_with_current_tenant_prefetch().exclude(exclusions)
     return ApartmentFilter(filters, apartments).qs
 
 
 def apartment_overview(semester: Semester):
     """
-    Return an overview of apartments occupancy for the current semester:
+    Return an overview of apartments occupancy for the selected semester:
     total, occupied, and vacant counts.
     """
-    
+
     apartments = Apartment.objects.all()
     total_apartments = apartments.count()
     available = apartment_available_units(semester_id=semester.pk).count()
@@ -70,34 +89,16 @@ def apartment_overview(semester: Semester):
 
 @apartment_not_found
 def apartment_for_update(apartment_id: int):
+    """Lock the apartment during payment processing"""
     return Apartment.objects.select_for_update().get(pk=apartment_id)
 
 
 @apartment_not_found
 def apartment_get_for(*, user: User, apartment_id: int):
+    """Filter the apartment based on the type of user first before fetch"""
     exclusions = ApartmentExclusions.for_user(user)
-    return Apartment.objects.exclude(exclusions).get(pk=apartment_id)
-
-
-def apartment_get_tenant_during(apartment_id: int, semester: Semester | None = None):
-    from semesters.selectors import semester_current
-
-    semester = semester or semester_current()
-    return (
-        Apartment.objects.prefetch_related("tenancy_set").get(pk=apartment_id, tenancy__semester_id=semester.pk).first()  # type: ignore
-    )
+    return get_base_qs_with_current_tenant_prefetch.exclude(exclusions).get(pk=apartment_id)
 
 
 def apartment_available_units(semester_id: int):
     return Apartment.objects.filter(rentable=True).exclude(tenancy__semester_id=semester_id)
-
-
-def apartment_occupancy_in_semester(*, apartment_id: int, semester_id: int):
-    try:
-        return Apartment.objects.annotate(
-            occupancy_count=models.Count("tenancy", filter=models.Q(tenancy__semester_id=semester_id))
-        ).get(id=apartment_id)
-    except Apartment.DoesNotExist:
-        raise NotFound({"apartment_id": [f"Apartment {apartment_id} not found"]})
-    except Semester.DoesNotExist:
-        raise NotFound({"semester_id": [f"Semester {semester_id} not found"]})
