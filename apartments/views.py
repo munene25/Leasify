d23251ba184d2a96859c50ed6a18b15cc7d8d7f0
munene25 <edmune25@gmail.com>
2026-04-21 12:55:56 +0300

    apartments.views: - added logging to apartment views.

--- apartments/views.py
+++ apartments/views.py
@@ -7,6 +7,10 @@ from rest_framework import serializers
 from common.pagination import get_paginated_response
 from common.permissions import check_perms
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from structlog import get_logger
+
+logger = get_logger("apartments.views")
+
 
 class ApartmentListCreateView(BaseAPIView):
     class FilterSerializer(serializers.Serializer):
@@ -37,6 +41,7 @@ class ApartmentListCreateView(BaseAPIView):
         incoming = self.validate_serializer(data=request.data)
         apartment = services.apartment_create(**incoming)
         outgoing = sc.ApartmentDetailSerializer(instance=apartment)
+        logger.info(f"Admin added a new apartment {str(apartment)}")
         return Response(data=outgoing.data, status=status.HTTP_201_CREATED)
 
 
@@ -46,25 +51,25 @@ class ApartmentDetailUpdateDeleteView(BaseAPIView):
 
 
     def get(self, request, apartment_id):
-        apartment = selectors.apartment_get_for(user=request.user, apartment_id=apartment_id)
-        outgoing = sc.ApartmentDetailSerializer(instance=apartment)
+        selected = selectors.apartment_get_for(user=request.user, apartment_id=apartment_id)
+        outgoing = sc.ApartmentDetailSerializer(instance=selected)
         return Response(status=status.HTTP_200_OK, data=outgoing.data)
 
     def patch(self, request, apartment_id):
         check_perms(request.user, "apartments.change_apartment")
 
         incoming = self.validate_serializer(data=request.data, partial=True)
-        apartment = selectors.apartment_get_for(user=request.user, apartment_id=apartment_id)
+        selected = selectors.apartment_get_for(user=request.user, apartment_id=apartment_id)
 
-        updated_apartment = services.apartment_update(apartment=apartment, **incoming)
-        outgoing  = sc.ApartmentDetailSerializer(instance=updated_apartment)
+        apartment = services.apartment_update(apartment=selected, **incoming)
+        outgoing  = sc.ApartmentDetailSerializer(instance=apartment)
         return Response(data=outgoing.data, status=status.HTTP_200_OK)
 
     def delete(self, request, apartment_id):
         check_perms(request.user, "apartments.delete_apartment")
-
         selected = selectors.apartment_get_for(user=request.user, apartment_id=apartment_id)
         services.apartment_delete(apartment=selected)
+        logger.warning(f"Admin deleted apartment {str(selected)}")
         return Response(status=status.HTTP_204_NO_CONTENT, data={"message": "Apartment deleted successfully"})
 
 
