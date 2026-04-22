89cedca0b82c879e3960775854e4d1d301ab1694
munene25 <edmune25@gmail.com>
2026-04-22 15:28:45 +0300

    apartment.serializer: - removed required arguments form apartment create as it's the default. - added a default to the rentable boolean field to true

--- apartments/serializer.py
+++ apartments/serializer.py
@@ -5,13 +5,13 @@ class ApartmentListSerializer(serializers.Serializer):
     apartment_name = serializers.CharField()
     rent = serializers.DecimalField(max_digits=10, decimal_places=2)
     rentable = serializers.BooleanField()
-    occupied = serializers.BooleanField()
+    occupied = serializers.BooleanField(allow_null=True)
 
 class ApartmentCreateSerializer(serializers.Serializer):
-    block = serializers.CharField(required=True)
-    unit_number = serializers.IntegerField(required=True)
-    rent = serializers.DecimalField(max_digits=10, decimal_places=2, required=True)
-    rentable = serializers.BooleanField(required=False)
+    block = serializers.CharField()
+    unit_number = serializers.IntegerField()
+    rent = serializers.DecimalField(max_digits=10, decimal_places=2)
+    rentable = serializers.BooleanField(required=False, default=True)
 
 class ApartmentUpdateSerializer(serializers.Serializer):
     block = serializers.CharField()
@@ -26,7 +26,7 @@ class ApartmentDetailSerializer(serializers.Serializer):
     rent = serializers.DecimalField(max_digits=10, decimal_places=2)
     rentable = serializers.BooleanField()
     created_at = serializers.DateTimeField()
-    current_tenant = serializers.CharField(source="user.full_name")
+    current_tenant = serializers.CharField(source="current_tenant.user.full_name", default=None)
 
     
 class ApartmentOverviewSerializer(serializers.Serializer):
