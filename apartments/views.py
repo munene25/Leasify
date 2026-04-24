0f770da1de1ff9eecbfefba789d7e1bf22850afd
munene25 <edmune25@gmail.com>
2026-04-24 14:05:22 +0300

    apartments.views: - reformatted code - Added docstrings for ListCreate and Detail views  - added permission checks for apartment get, might switch up to different serializers based on which user is requesting. This would allow tenants to view apartment details. For now, there is no point in viewing the apartment detail for other users apart from

--- apartments/views.py
+++ apartments/views.py
@@ -1,18 +1,23 @@
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from common.views import BaseAPIView
+from rest_framework import serializers
 from rest_framework import status
+from common.views import BaseAPIView
+from common.permissions import check_perms
+from common.pagination import get_paginated_response
 from semesters.selectors import semester_current
 from apartments import selectors, services, serializer as sc
-from rest_framework import serializers
-from common.pagination import get_paginated_response
-from common.permissions import check_perms
-from rest_framework.permissions import IsAuthenticated, AllowAny
 from structlog import get_logger
 
 logger = get_logger("apartments.views")
 
 
 class ApartmentListCreateView(BaseAPIView):
+    """
+    Routes for listing the apartments adding apartments.
+    Apartments are filtered based on which user is viewing.
+    All apartments viewable by priviledged users, and rentable apartments for the rest.
+    """
     class FilterSerializer(serializers.Serializer):
         search = serializers.CharField()
         rentable = serializers.BooleanField(allow_null=True)
@@ -22,19 +27,16 @@ class ApartmentListCreateView(BaseAPIView):
 
     serializer_class = sc.ApartmentCreateSerializer
     filter_class = FilterSerializer
-    
+
     def get_permissions(self):
-        """Only authenticated users during apartment creation, otherwise open to all."""    
+        """Only authenticated users during apartment creation, otherwise open to all."""
         return [IsAuthenticated()] if self.request.method == "POST" else [AllowAny()]
 
     def get(self, request):
         filters = self.validate_filter(data=request.query_params)
         qs = selectors.apartment_list_for(user=request.user, filters=filters)
         return get_paginated_response(
-            serializer_class=sc.ApartmentListSerializer, 
-            queryset=qs, 
-            request=request, 
-            view=self
+            serializer_class=sc.ApartmentListSerializer, queryset=qs, request=request, view=self
         )
 
     def post(self, request):
@@ -46,11 +48,22 @@ class ApartmentListCreateView(BaseAPIView):
 
 
 class ApartmentDetailUpdateDeleteView(BaseAPIView):
-    serializer_class = sc.ApartmentUpdateSerializer
-    permission_classes =[IsAuthenticated]
+    """
+    All the routes here are priviledged.
+    No need to allow users to view apartment details if no extra usefull information is being provided to unauathenticated users.
+    """
 
+    serializer_class = sc.ApartmentUpdateSerializer
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, apartment_id):
+        """
+        At this point, there is no new information provided in detail other than what the admin should view ie: current_tenant.
+        Therefore it's better to just have it as an admin route.
+        If anything changes, better to add view_apartment to the tenant's permission then serialize the instance differently based on the user.
+        If this is the case, user rank based on their permission is more viable.
+        """
+        check_perms(request.user, "apartments.view_apartment")
         selected = selectors.apartment_get_for(user=request.user, apartment_id=apartment_id)
         outgoing = sc.ApartmentDetailSerializer(instance=selected)
         return Response(status=status.HTTP_200_OK, data=outgoing.data)
@@ -62,14 +75,16 @@ class ApartmentDetailUpdateDeleteView(BaseAPIView):
         selected = selectors.apartment_get_for(user=request.user, apartment_id=apartment_id)
 
         apartment = services.apartment_update(apartment=selected, **incoming)
-        outgoing  = sc.ApartmentDetailSerializer(instance=apartment)
+        outgoing = sc.ApartmentDetailSerializer(instance=apartment)
         return Response(data=outgoing.data, status=status.HTTP_200_OK)
 
     def delete(self, request, apartment_id):
         check_perms(request.user, "apartments.delete_apartment")
         selected = selectors.apartment_get_for(user=request.user, apartment_id=apartment_id)
         services.apartment_delete(selected)
-        return Response(status=status.HTTP_204_NO_CONTENT, data={"message": f"Apartment {str(selected)} deleted successfully"})
+        return Response(
+            status=status.HTTP_204_NO_CONTENT, data={"message": f"Apartment {str(selected)} deleted successfully"}
+        )
 
 
 class ApartmentOverviewView(BaseAPIView):
