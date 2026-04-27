80c7125deac10b8ef1e7c9491ac4473b6d92d82e
munene25 <edmune25@gmail.com>
2026-04-27 14:09:05 +0300

    apartments.serializer: - added block validation and conversion mixin.

--- apartments/serializer.py
+++ apartments/serializer.py
@@ -1,5 +1,13 @@
 from rest_framework import serializers
+from apartments.models import Apartment
 
+class BlockValidator:
+    def validate_block(self, data: str):
+        string = data.upper()
+        if string not in Apartment.ApartmentChoices.values:
+            raise serializers.ValidationError(f"Only available choices are {Apartment.ApartmentChoices.values})")
+        return string
+    
 class ApartmentListSerializer(serializers.Serializer):
     apartment_id = serializers.IntegerField(source="pk")
     apartment_name = serializers.CharField()
@@ -7,17 +15,19 @@ class ApartmentListSerializer(serializers.Serializer):
     rentable = serializers.BooleanField()
     occupied = serializers.BooleanField(allow_null=True)
 
-class ApartmentCreateSerializer(serializers.Serializer):
+class ApartmentCreateSerializer(serializers.Serializer, BlockValidator):
     block = serializers.CharField()
     unit_number = serializers.IntegerField()
     rent = serializers.DecimalField(max_digits=10, decimal_places=2)
     rentable = serializers.BooleanField(required=False, default=True)
 
-class ApartmentUpdateSerializer(serializers.Serializer):
-    block = serializers.CharField()
+class ApartmentUpdateSerializer(serializers.Serializer, BlockValidator):
+    block = serializers.ChoiceField(choices=Apartment.ApartmentChoices.values)
     unit_number = serializers.IntegerField()
     rent = serializers.DecimalField(max_digits=10, decimal_places=2)
     rentable = serializers.BooleanField()
+
+
 
 class ApartmentDetailSerializer(serializers.Serializer):
     apartment_id = serializers.IntegerField(source="pk")
