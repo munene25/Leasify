bd0186e27d20f4ac2c6ac80b6f2ccc81d17ad8ec
munene25 <edmune25@gmail.com>
2026-04-29 13:46:23 +0300

    tenancy.selectors: - added a base_qs to better select the related user on each qs. - added a current_tenant qs to be used in apts

--- tenancy/selectors.py
+++ tenancy/selectors.py
@@ -6,9 +6,12 @@ prefetch_user_apt_sem = Prefetch(
     "tenancy_set",
     Tenancy.objects.all().select_related("user", "apartment", "semester"),
 )
+base_qs = Tenancy.objects.select_related("user")
 
-def tenancy_list()-> QuerySet:
+
+def tenancy_list() -> QuerySet:
     return Tenancy.objects.select_related("apartment", "user").all()
+
 
 def tenancy_for_update(tenancy_id: int) -> Tenancy:
     """Fetches the tenancy and related apartment for update"""
@@ -17,11 +20,13 @@ def tenancy_for_update(tenancy_id: int) -> Tenancy:
     except Tenancy.DoesNotExist:
         raise NotFound({"tenancy_id": "Tenancy not found"})
 
+
 def tenancy_get_by_user_id(*, user_id: int):
     try:
         return Tenancy.objects.select_related("user").get(user_id=user_id)
     except Tenancy.DoesNotExist:
         raise NotFound({"tenancy_id": ["Tenancy not found"]})
+
 
 def tenancy_get_by_apartment_id(apartment_id: int):
     return Tenancy.objects.select_related("apartment").filter(apartment=apartment_id)
@@ -29,9 +34,21 @@ def tenancy_get_by_apartment_id(apartment_id: int):
 
 def tenancy_get_by_id(tenancy_id: int):
     try:
-        return Tenancy.objects.select_related("apartment", "semester", "user").get(
-            pk=tenancy_id
-        )
+        return Tenancy.objects.select_related("apartment", "semester", "user").get(pk=tenancy_id)
     except Tenancy.DoesNotExist:
         raise NotFound({"tenancy_id": "Tenancy not found"})
 
+
+def tenancy_current() -> QuerySet[Tenancy]:
+    """
+    Filter out the current tenant in that semester if the semester exists 
+    It is important to decouple the semester existing or not in order to get the current tenant.
+    """
+    from semesters.selectors import semester_current
+    
+    # better to use the cached object through current semester selector
+    try:
+        sem = semester_current()
+        return base_qs.filter(semester_id=sem.pk)
+    except NotFound:
+        return Tenancy.objects.none()
