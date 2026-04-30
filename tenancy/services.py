47c255d51babbcb9e37e43b6f3fd0f2faf7d2f50
munene25 <edmune25@gmail.com>
2026-04-30 15:27:44 +0300

    tenancy.services: - huge sweeping changes to how the structure of the code. - now is function based

--- tenancy/services.py
+++ tenancy/services.py
@@ -1,124 +1,139 @@
 from decimal import Decimal
+from typing import TypedDict, Unpack, TYPE_CHECKING
 from django.db import transaction
 from django.utils import timezone
-from django.contrib.auth.models import Group
 from rest_framework.exceptions import ValidationError
 from tenancy.models import Tenancy
-from .selectors import tenancy_for_update
+from tenancy.selectors import tenancy_lock
 from semesters import selectors as sem_selectors
 from apartments.selectors import apartment_for_update
-from users import selectors as user_selectors
+from users.services import user_set_role
 from payments.services import PaymentCreateService
 from users.services import user_set_role
+from common.exceptions import SemesterEndedError, ApartmentOccupiedError
 
-class TenancyService:
-    def __init__(self, tenancy_id: int | None = None):
-        self.EDITABLE_FIELDS = {"semester_id", "apartment_id"}
-        self.tenancy_id = tenancy_id
+
+if TYPE_CHECKING:
+    from users.models import User
+    from semesters.models import Semester
+    from apartments.models import Apartment
+
+class TenancyUpdate(TypedDict, total=False):
+    semester: Semester
+    apartment: Apartment
+
+def validate_apartment_and_semester(apartment: "Apartment", semester: "Semester") -> None:
+    """
+    Only future and current semesters should be available for booking
+
+    The apartment rentable flag is a global lock allowing or disallowing tenants to book.
+    Non rentable apartments are already filtered out of tenants and guest querysets.
+    Nontheless we should explicitly check first if the apartment is availalbe for rent.
+    And fallback to occupancy in cases where a manager is assigning an apartment.
+    This is still enforced at the database level with unique constraints.
+    """
+        
+    if semester.has_ended:
+        raise SemesterEndedError()
+    if not apartment.rentable:
+        raise ValidationError({"apartment_id": "Apartment not available for renting"})
+    if apartment.tenancy_set.filter(semester_id=semester.pk).exists():
+        raise ApartmentOccupiedError()
     
-    def tenancy_get_locked(self) -> Tenancy:
-        if not self.tenancy_id:
-            raise ValidationError({"tenancy_id": "Tenancy not provided"})
-        return tenancy_for_update(self.tenancy_id)
+
+@transaction.atomic
+def create(*, user: "User", apartment: "Apartment", semester: "Semester", total_paid: Decimal = Decimal(0)) -> Tenancy:
+    """
+    Create a tenancy record for a user, apartment, and semester.
+    Add user to tenant group.
     
-    def _validate_semester_dates(self, semester_id: int) -> None:
-        selected_semester = sem_selectors.semester_get_by_id(semester_id)
-        now = timezone.now().date()
-        if now > selected_semester.end_date:
-            err = "Invalid semester. Please select a semester that has not yet concluded"
-            raise ValidationError({"semester_id": [err]})
-        
-    def _validate_apartment_vacancy(self, *, apartment, semester_id: int) -> None:
-        if apartment.rentable == False and apartment.tenancy_set.filter(
-            semester_id=semester_id,
-        ).exists():
-            raise ValidationError({"apartment_id": ["Aparment is not available in selected semester"]})
+    :param self: TenancyService instance
+    :param user: The user occupying the apartment
+    :type user: User
+    :param apartment: Apartment to be booked. will be flagged not rentable if successful to prevent slotting. 
+    :type apartment: Apartment
+    :param semester: Semester associated with the tenancy
+    :type semester: Semester
+    :param total_paid: Total sum of money received from the tenant.
+    :type total_paid: Decimal
+    :return: Returns the created tenancy record
+    :rtype: Tenancy
+    """
+    from django.contrib.auth.models import Group
 
-    @transaction.atomic
-    def create(self, user_id: int, apartment_id: int, semester_id : int) -> Tenancy:
-        """
-        Create a tenancy record for a user, apartment, and semester.
-        
-        :param self: TenancyService instance
-        :param user_id: The user associated with the tenancy
-        :type user_id: int
-        :param apartment_id: Apartment associated with the tenancy will be locked during the transaction
-        :type apartment_id: int
-        :param semester_id: Semester associated with the tenancy
-        :type semester_id: int
-        :return: Returns the created tenancy record
-        :rtype: Tenancy
-        """
-        # Lock apartment and validate semester
-        self._validate_semester_dates(semester_id)
-        apt = apartment_for_update(apartment_id)
-        self._validate_apartment_vacancy(
-            semester_id=semester_id,
-            apartment=apt
-        )
+    # First validate apartment and semester availablity
+    validate_apartment_and_semester(apartment, semester)
 
-        # Create tenancy
-        tenancy = Tenancy(
-            user_id=user_id,
-            apartment_id=apartment_id,
-            semester_id=semester_id,
-            total_paid=Decimal("0.00"),
-        )
-        tenancy.full_clean()
-        tenancy.save()
+    # Create tenancy
+    tenancy = Tenancy(
+        user=user,
+        apartment=apartment,
+        semester=semester,
+        total_paid=total_paid,
+    )
+    tenancy.full_clean()
+    tenancy.save()
 
-        # Add user to Tenant group
-        user = user_selectors.user_get(user_id)
-        tenancy_group, _ = Group.objects.get_or_create(name="tenant")
-        if not user.groups.filter(name="tenant").exists():
-            user_set_role(user=user, role=tenancy_group)
-        return tenancy
+    # Add user to Tenant group
+    
+    if not user.groups.filter(name="tenant").exists():
+        user_set_role(user=user, role=Group.objects.get(name="tenant"))
+    return tenancy
 
-    @transaction.atomic
-    def update(self, **kwargs: int ) -> Tenancy:
-        """
-        Update tenant's semester or apartment
-        
-        :param self: TenancyService instance
-        :param kwargs: semester_id: int, apartment_id: int
-        :type kwargs: int
-        :return: Description
-        :rtype: Tenancy
-        """
-        tenancy = self.tenancy_get_locked()
+@transaction.atomic
+def tenancy_update(tenancy: Tenancy, **kwargs: Unpack[TenancyUpdate] ) -> Tenancy:
+    """
+    Update tenant's semester or apartment
+    
+    :param tenancy: Tenant object being updated
+    :type tenancy: Tenancy
 
-        update_fields = {
-            k: v
-            for k, v in kwargs.items()
-            if k in self.EDITABLE_FIELDS and getattr(tenancy, k) != v
-        }
-        if not update_fields:
-            return tenancy
-        
-        # Lock new apartment if one is provided else use the associated locked apartment
-        apt_id = update_fields.get("apartment_id")
-        apartment = apartment_for_update(apt_id) if apt_id else tenancy.apartment
-        
-        sem_id =  update_fields.get("semester_id", tenancy.semester_id) # type: ignore
+    :param kwargs: semester: Semester, apartment: Apartment
+    :type kwargs: Any
+    
+    :return: Description
+    :rtype: Tenancy
+    """
 
-        # Validate new semester - apartment combination
-        self._validate_apartment_vacancy(apartment=apartment, semester_id=sem_id) # type: ignore
-
-        # individual updates for semester and apartment
-        for field, value in update_fields.items():
-            setattr(tenancy, field, value)
-        tenancy.full_clean()
-        tenancy.save(update_fields=list(update_fields.keys()))
+    editable_fields = {"semester", "apartment"}
+    update_fields = {
+        k: v
+        for k, v in kwargs.items()
+        if k in editable_fields and getattr(tenancy, k) != v
+    }
+    if not update_fields:
         return tenancy
     
-    @transaction.atomic
-    def delete(self) -> None:
-        tenancy = self.tenancy_get_locked()
-        if tenancy.total_paid > Decimal(0):
-            PaymentCreateService(
-                amount=tenancy.total_paid,
-                transaction_type="credit",
-                tenancy_id=tenancy.pk,
-                initiator="system",
-            ).create()
-        tenancy.delete()
+    apartment = update_fields.get("apartment") or tenancy.apartment
+    semester = update_fields.get("semester") or tenancy.semester
+    validate_apartment_and_semester(apartment, semester) #type: ignore
+
+    # individual updates for semester and apartment
+    for field, value in update_fields.items():
+        setattr(tenancy, field, value)
+
+    tenancy.full_clean()
+    tenancy.save(update_fields=list(update_fields.keys()))
+    return tenancy
+
+@transaction.atomic
+def delete(tenancy: Tenancy) -> None:
+    """Delete tenant record and credit the payments"""
+
+    if tenancy.total_paid > Decimal(0):
+        PaymentCreateService(
+            amount=tenancy.total_paid,
+            transaction_type="credit",
+            tenancy_id=tenancy.pk,
+            initiator="system",
+        ).create()
+    tenancy.delete()
+
+
+
+
+    
+
+    
+    
+
