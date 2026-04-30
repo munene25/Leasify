dd06aad68eef0b13f2a04bd2c9d01afbc346f163
munene25 <edmune25@gmail.com>
2026-04-30 15:23:00 +0300

    apartments.admin: - renamed occupied field to is_occupied.

--- apartments/admin.py
+++ apartments/admin.py
@@ -8,32 +8,32 @@ from django.utils.html import format_html
 
 logger = get_logger("apartments.admin")
 
+
 @admin.register(Apartment)
 class ApartmentAdmin(admin.ModelAdmin):
-    list_display = ("apartment_name", "rentable", "rent", "occupied")
-    list_filter = ("rentable", "block",)
+    list_display = ("apartment_name", "rentable", "rent", "is_occupied")
+    list_filter = ("rentable", "block")
 
     search_fields = ("unit_number",)
-    
+
     fields = (
         "block",
         "unit_number",
         "rentable",
         "rent",
-        "occupied",
+        "is_occupied",
         "created_at",
         "current_tenant",
     )
 
-    readonly_fields = ("occupied", "current_tenant", "created_at")
-    
+    readonly_fields = ("is_occupied", "current_tenant", "created_at")
     actions = ["make_rentable", "make_unrentable"]
 
-    def occupied(self, obj):
-        return obj.occupied
-    
-    occupied.boolean = True
-    occupied.short_description = "Currently occupied"
+    def is_occupied(self, obj):
+        return obj.is_occupied
+
+    is_occupied.boolean = True
+    is_occupied.short_description = "Currently is_occupied"
 
     def current_tenant(self, obj):
         tenant = obj.current_tenant
@@ -48,13 +48,8 @@ class ApartmentAdmin(admin.ModelAdmin):
     def get_queryset(self, request: HttpRequest) -> QuerySet:
         from apartments.selectors import tenancy_prefetch
 
-        return (
-            super()
-            .get_queryset(request)
-            .prefetch_related(tenancy_prefetch())
-        )
+        return super().get_queryset(request).prefetch_related(tenancy_prefetch())
 
-    
     @admin.action(description="Mark selected apartments as rentable")
     def make_rentable(self, request, queryset):
         queryset.update(rentable=True)
@@ -64,13 +59,13 @@ class ApartmentAdmin(admin.ModelAdmin):
     def make_unrentable(self, request, queryset):
         queryset.update(rentable=False)
         logger.info("apartments_made_unrentable", apartments=queryset.value_list("id", flat=True))
-    
-    
+
     def save_model(self, request, obj, form, change) -> None:
         super().save_model(request, obj, form, change)
         state = {True: "apartment_updated", False: "apartment_created"}[change]
         logger.info(state, apartment_name=obj.apartment_name)
-    
+
     def delete_model(self, request: HttpRequest, obj) -> None:
         super().delete_model(request, obj)
         logger.warning("apartment_deleted", apartment_name=obj.apartment_name)
+    
