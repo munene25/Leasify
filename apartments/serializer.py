7c1d20092524b78054c030bf729cbc5550e17b12
munene25 <edmune25@gmail.com>
2026-04-30 15:24:28 +0300

    apartments.serializer: - renamed attribute occupied to is_occupied

--- apartments/serializer.py
+++ apartments/serializer.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from apartments.models import Apartment
 
-    
+
 class ApartmentListSerializer(serializers.Serializer):
     apartment_id = serializers.IntegerField(source="pk")
     apartment_name = serializers.CharField()
     rent = serializers.DecimalField(max_digits=10, decimal_places=2)
     rentable = serializers.BooleanField()
-    occupied = serializers.BooleanField(allow_null=True)
+    is_occupied = serializers.BooleanField(allow_null=True)
+
 
 class ApartmentCreateSerializer(serializers.Serializer):
     block = serializers.ChoiceField(choices=Apartment.ApartmentChoices.values)
@@ -15,12 +16,12 @@ class ApartmentCreateSerializer(serializers.Serializer):
     rent = serializers.DecimalField(max_digits=10, decimal_places=2)
     rentable = serializers.BooleanField(required=False, default=True)
 
+
 class ApartmentUpdateSerializer(serializers.Serializer):
     block = serializers.ChoiceField(choices=Apartment.ApartmentChoices.values)
     unit_number = serializers.IntegerField()
     rent = serializers.DecimalField(max_digits=10, decimal_places=2)
     rentable = serializers.BooleanField()
-
 
 
 class ApartmentDetailSerializer(serializers.Serializer):
@@ -32,7 +33,7 @@ class ApartmentDetailSerializer(serializers.Serializer):
     created_at = serializers.DateTimeField()
     current_tenant = serializers.CharField(source="current_tenant.user.full_name", default=None)
 
-    
+
 class ApartmentOverviewSerializer(serializers.Serializer):
     total_apartments = serializers.IntegerField()
     occupied = serializers.IntegerField()
