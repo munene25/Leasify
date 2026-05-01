dce050a5b39e788903e14989eed8440d51ffc8ba
munene25 <edmune25@gmail.com>
2026-05-01 17:17:41 +0300

    common.helpers: - added type annotations for correct return types for the wrapped func

--- common/helpers.py
+++ common/helpers.py
@@ -1,17 +1,19 @@
 from functools import wraps
-from typing import Callable, Any
+from typing import Callable, Any, TypeVar
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.exceptions import NotFound
 
 
-def raise_not_found(field: str, message: str):
+R = TypeVar("R")
+
+def raise_not_found(field: str, message: str) -> Callable[[Callable[..., R]], Callable[..., R]]:
     """
-    A clean wrapper for attatching error messages to not found exceptions
+    A clean wrapper for attaching error messages to not found exceptions
     """
 
-    def wrapper(func: Callable[..., Any]):
+    def wrapper(func: Callable[..., R]) -> Callable[..., R]:
         @wraps(func)
-        def wrapped(*args, **kwargs):
+        def wrapped(*args, **kwargs) -> R:
             try:
                 return func(*args, **kwargs)
             except ObjectDoesNotExist as exc:
