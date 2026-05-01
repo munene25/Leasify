df770fb920bcc8da153484f6d24e279e42558997
munene25 <edmune25@gmail.com>
2026-05-01 17:47:26 +0300

    tenancy.selectors: - mad improvements to tenancy_lists and locking functions - added decorators for ease of catching not founds

--- tenancy/selectors.py
+++ tenancy/selectors.py
@@ -1,54 +1,88 @@
-from django.db.models import Prefetch, QuerySet
-from .models import Tenancy
-from rest_framework.exceptions import NotFound
+from typing import Any, TYPE_CHECKING
+from django.http.request import QueryDict
+from django.db import models
+from tenancy.models import Tenancy
+from common.helpers import raise_not_found
 
-prefetch_user_apt_sem = Prefetch(
-    "tenancy_set",
-    Tenancy.objects.all().select_related("user", "apartment", "semester"),
-)
-base_qs = Tenancy.objects.select_related("user")
+if TYPE_CHECKING:
+    from semesters.models import Semester
 
-
-def tenancy_list() -> QuerySet:
-    return Tenancy.objects.select_related("apartment", "user").all()
+BASE_QS = Tenancy.objects.select_related("user", "apartment")
 
 
+
+tenancy_not_found = raise_not_found("tenancy_id", "Tenancy does not exist.")
+
+
+def tenancy_list(filters: QueryDict | dict[str, Any]) -> models.QuerySet[Tenancy]:
+    import django_filters
+    from django.db.models import Q, F
+
+    class TenancyFilter(django_filters.FilterSet):
+        class Meta:
+            model = Tenancy
+            fields = ("total_paid", )
+
+        search = django_filters.CharFilter(method="search_fields")
+        cleared = django_filters.BooleanFilter(method="is_cleared")
+        period = django_filters.CharFilter(method="filter_period")
+        
+        def search_fields(self, queryset, name, value: str):
+            q = Q()
+            if value.isdigit():
+                q |= Q(apartment__unit_number=value)
+            else:
+                q |= Q(apartment__block__icontains=value)
+                q |= Q(user__first_name__icontains=value)
+                q |= Q(user__last_name__icontains=value)
+            return queryset.filter(q)
+        
+        def is_cleared(self, queryset, name, value: bool):
+            f = F("apartment__rent")   
+            q = Q(total_paid__gte=f) if value else Q(total_paid__lt=f)
+            return queryset.filter(q)
+
+        def filter_period(self, queryset, name, value: str):
+            from semesters.models import Semester
+
+            # Need to filter based on year, start_month or end_month:
+            start_date, end_date = Semester.get_date_range(value)
+
+            return queryset.filter(semester__end_date__gte=start_date, semester__start_date__lte=end_date)  
+
+    tenancies = BASE_QS.select_related("semester").all()
+    return TenancyFilter(filters, tenancies).qs
+
+@tenancy_not_found
 def tenancy_lock(tenancy_id: int) -> Tenancy:
-    """Fetches the tenancy and related apartment for update"""
-    try:
-        return Tenancy.objects.select_related("apartment", "user").select_for_update().get(pk=tenancy_id)
-    except Tenancy.DoesNotExist:
-        raise NotFound({"tenancy_id": "Tenancy not found"})
-
-
-def tenancy_get_by_user_id(*, user_id: int):
-    try:
-        return Tenancy.objects.select_related("user").get(user_id=user_id)
-    except Tenancy.DoesNotExist:
-        raise NotFound({"tenancy_id": ["Tenancy not found"]})
-
-
-def tenancy_get_by_apartment_id(apartment_id: int):
-    return Tenancy.objects.select_related("apartment").filter(apartment=apartment_id)
-
-
-def tenancy_get_by_id(tenancy_id: int):
-    try:
-        return Tenancy.objects.select_related("apartment", "semester", "user").get(pk=tenancy_id)
-    except Tenancy.DoesNotExist:
-        raise NotFound({"tenancy_id": "Tenancy not found"})
-
-
-def tenancy_current() -> QuerySet[Tenancy]:
     """
-    Filter out the current tenant in that semester if the semester exists 
+    Fetches the tenancy and related apartment for update
+    Important: It selects the apartment for update as well for payment computations
+    """
+
+    return Tenancy.objects.select_related("apartment").select_for_update().get(pk=tenancy_id)
+
+@tenancy_not_found
+def tenancy_get(tenancy_id: int):
+    BASE_QS.get(pk=tenancy_id)
+
+
+def tenancy_for_semester(semester: "Semester | None" = None) -> models.QuerySet[Tenancy]:
+    """
+    Defaults to current_semester.
+    Filter out the tenants in that semester.
+    If semester does not exists, it will default no tenant
     It is important to decouple the semester existing or not in order to get the current tenant.
     """
     from semesters.selectors import semester_current
-    
-    # better to use the cached object through current semester selector
+    from rest_framework.exceptions import NotFound
+
     try:
-        sem = semester_current()
-        return base_qs.filter(semester_id=sem.pk)
+        # better to use the cached object through current_semester selector
+        sem = semester or semester_current()
+        return Tenancy.objects.select_related("user").filter(semester_id=sem.pk)
     except NotFound:
+        # This is in cases where the semester is none
         return Tenancy.objects.none()
+
+current_tenant_prefetch = lambda: models.Prefetch("tenancy_set", tenancy_for_semester(), to_attr="_current_tenant")
