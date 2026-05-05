ec5de42020e7ee9dd1902c1d98007aabccb8db85
munene25 <edmune25@gmail.com>
2026-05-05 17:39:21 +0300

    tenancy.views: - refactored views to use the current services and selectors

--- tenancy/views.py
+++ tenancy/views.py
@@ -1,44 +1,60 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
 from common.views import BaseAPIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, serializers
+from rest_framework.permissions import IsAuthenticated
+from common.permissions import check_perms
+from common.pagination import get_paginated_response
 from tenancy import services as sr, serializer as sc, selectors as sl
 
-class TenancyListCreateView(BaseAPIView):
-    serializer_class = TenancyCreateSerializer
+if TYPE_CHECKING:
+    from rest_framework.request import Request
+    from rest_framework.response import Response
 
-    def get(self, request) -> Response:
-        tenancies = tenancy_list()
-        serializer = TenancyListSerializer(instance=tenancies, many=True)
-        return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+class TenancyListCreateView(BaseAPIView):
+    class FilterClass(serializers.Serializer):
+        period = serializers.CharField()
+        search = serializers.CharField()
+        cleared = serializers.BooleanField()
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = sc.TenancyCreateSerializer
+    filter_class = FilterClass
+
+    def get(self, request: Request) -> Response:
+        check_perms(request.user, "tenancy.view_tenancy")
+        filters = self.validate_filter(data=request.query_params)
+        qs = sl.tenancy_list_for(user=request.user, filters=filters)
+        return get_paginated_response(serializer_class=sc.TenancyListSerializer, queryset=qs, request=request, view=self)
 
     def post(self, request) -> Response:
-        data = self.validate_serializer(data=request.data)
-        service = TenancyService()
-        service.create(**data)
-        return Response(status=status.HTTP_201_CREATED)
+        check_perms(request.user, "tenancy.create_tenancy")
+        incoming = self.validate_serializer(data=request.data)
+        tenancy = sr.tenancy_create(**incoming)
+        outgoing = sc.TenancyDetailSerializer(instance=tenancy)
+        return Response(data=outgoing.data ,status=status.HTTP_201_CREATED)
 
 
 class TenancyDetailUpdateDestroyView(BaseAPIView):
-    serializer_class = TenancyUpdateSerializer
+    serializer_class = sc.TenancyUpdateSerializer
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, tenancy_id: int) -> Response:
-        tenancy = tenancy_get_by_id(tenancy_id)
-        serializer = TenancyDetailSerializer(instance=tenancy)
-        return Response(status=status.HTTP_200_OK, data=serializer.data)
-
-    def put(self, request, tenancy_id: int) -> Response:
-        data = self.validate_serializer(data=request.data)
-        service = TenancyService(tenancy_id)
-        service.update(**data)
-        return Response(status=status.HTTP_200_OK)
+        check_perms(request.user, "tenancy.view_tenancy")
+        tenancy = sl.tenancy_get_for(request.user, tenancy_id)
+        outgoing = sc.TenancyDetailSerializer(instance=tenancy)
+        return Response(status=status.HTTP_200_OK, data=outgoing.data)
 
     def patch(self, request, tenancy_id: int) -> Response:
-        data = self.validate_serializer(data=request.data, partial=True)
-        service = TenancyService(tenancy_id)
-        service.update(**data)
-        return Response(status=status.HTTP_200_OK)
+        check_perms(request.user, "tenancy.change_tenancy")
+        incoming = self.validate_serializer(data=request.data)
+        selected = sl.tenancy_get_for(request.user, tenancy_id)
+        updated = sr.tenancy_update(selected, **incoming)
+        outgoing = sc.TenancyDetailSerializer(instance=updated)
+        return Response(data=outgoing.data, status=status.HTTP_200_OK)
 
     def delete(self, request, tenancy_id: int) -> Response:
-        service = TenancyService(tenancy_id)
-        service.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        check_perms(request.user, "tenancy.delete_tenancy")
+        selected = sl.tenancy_get_for(request.user , tenancy_id)
+        return Response(data={"message": "Tenancy Deleted"}, status=status.HTTP_204_NO_CONTENT)
