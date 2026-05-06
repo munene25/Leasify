1ca264c8030c315e79356cc2718a9e1be8ea647a
munene25 <edmune25@gmail.com>
2026-05-06 15:48:50 +0300

    tenancy.selectors: - reformatted code - added a tenancy_recent selector

--- tenancy/selectors.py
+++ tenancy/selectors.py
@@ -11,7 +11,7 @@ if TYPE_CHECKING:
 
 
 class TenancyFilterPolicy(FilteringPolicy):
-    SUPERUSER =  MANAGER = CARETAKER = Q()
+    SUPERUSER = MANAGER = CARETAKER = Q()
     TENANT = REGULAR = lambda user: Q(user_id=user.pk)
 
 
@@ -21,19 +21,18 @@ tenancy_not_found = raise_not_found("tenancy_id", "Tenancy does not exist.")
 
 
 def tenancy_list_for(user: "User", filters: QueryDict | dict[str, Any]) -> QuerySet[Tenancy]:
-    
-    
+
     import django_filters
 
     class TenancyFilter(django_filters.FilterSet):
         class Meta:
             model = Tenancy
-            fields = ("total_paid", )
+            fields = ("total_paid",)
 
         search = django_filters.CharFilter(method="search_fields")
         cleared = django_filters.BooleanFilter(method="is_cleared")
         period = django_filters.CharFilter(method="filter_period")
-        
+
         def search_fields(self, queryset, name, value: str):
             q = Q()
             if value.isdigit():
@@ -43,9 +42,9 @@ def tenancy_list_for(user: "User", filters: QueryDict | dict[str, Any]) -> Query
                 q |= Q(user__first_name__icontains=value)
                 q |= Q(user__last_name__icontains=value)
             return queryset.filter(q)
-        
+
         def is_cleared(self, queryset, name, value: bool):
-            f = F("apartment__rent")   
+            f = F("apartment__rent")
             q = Q(total_paid__gte=f) if value else Q(total_paid__lt=f)
             return queryset.filter(q)
 
@@ -55,11 +54,12 @@ def tenancy_list_for(user: "User", filters: QueryDict | dict[str, Any]) -> Query
             # Need to filter based on year, start_month or end_month:
             start_date, end_date = Semester.get_date_range(value)
 
-            return queryset.filter(semester__end_date__gte=start_date, semester__start_date__lte=end_date)  
+            return queryset.filter(semester__end_date__gte=start_date, semester__start_date__lte=end_date)
 
     t_filters = TenancyFilterPolicy.for_user(user)
     tenancies = BASE_QS.select_related("semester").filter(t_filters)
     return TenancyFilter(filters, tenancies).qs
+
 
 @tenancy_not_found
 def tenancy_lock(tenancy_id: int) -> Tenancy:
@@ -70,12 +70,27 @@ def tenancy_lock(tenancy_id: int) -> Tenancy:
 
     return Tenancy.objects.select_related("apartment").select_for_update().get(pk=tenancy_id)
 
+
 @tenancy_not_found
 def tenancy_get_for(user: "User", tenancy_id: int) -> Tenancy:
     """Will filter out the tenancies viewable only to the user based on the policy"""
 
     t_filters = TenancyFilterPolicy.for_user(user)
     return BASE_QS.filter(t_filters).get(pk=tenancy_id)
+
+
+def tenancy_recent(user: "User") -> Tenancy | None:
+    from semesters.models import GRACE_PERIOD
+    from django.utils import timezone
+
+    now = timezone.now().date()
+    extension = now - GRACE_PERIOD
+    return (
+        Tenancy.objects.select_related("semester")
+        .order_by("-semester__start_date")
+        .filter(user_id=user.pk, semester__start_date__lte=now, semester__end_date__gte=extension)
+        .first()
+    )
 
 
 def tenancy_for_semester(semester: "Semester | None" = None) -> QuerySet[Tenancy]:
@@ -95,5 +110,6 @@ def tenancy_for_semester(semester: "Semester | None" = None) -> QuerySet[Tenancy
     except NotFound:
         # This is in cases where the semester is none
         return Tenancy.objects.none()
+
 
 current_tenant_prefetch = lambda: Prefetch("tenancy_set", tenancy_for_semester(), to_attr="_current_tenant")
