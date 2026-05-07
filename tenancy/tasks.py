4d3ac0d94694da4bfd6993c6dcda1f322115ec2c
munene25 <edmune25@gmail.com>
2026-05-07 17:25:02 +0300

    tenancy.tasks: - i dont even know why this file exists.

--- tenancy/tasks.py
+++ tenancy/tasks.py
@@ -1,9 +1,7 @@
-import time
 from celery import shared_task
-from . import selectors
+from tenancy import selectors as sl
 
 @shared_task(ignore_results=False)
 def show_detail(tenancy_id):
-    time.sleep(10)
-    tenancy = selectors.tenancy_get_by_id(tenancy_id=tenancy_id)
+    tenancy = sl.tenancy_get(tenancy_id=tenancy_id)
     return f'User {tenancy.user.get_full_name()} just requested this page'
