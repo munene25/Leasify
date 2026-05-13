3481e164f099c3c15001e05494d47c214625a126
munene25 <edmune25@gmail.com>
2026-05-13 13:12:57 +0300

    scripts.testing: - test for how the apartments are filtered based on user role

--- scripts/testing.py
+++ scripts/testing.py
@@ -1,42 +1,25 @@
 from freezegun import freeze_time
 from datetime import datetime, timedelta, timezone
+from pprint import pprint
 from django.db import models, connection
 from django.utils import timezone as tz
 from django.contrib.auth.models import Permission
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
-from users.models import User
-from apartments.models import Apartment
-from semesters import models, selectors as sem_selectors
-from semesters.selectors import semester_current
-from payments.models import Payment
-from tenancy.models import Tenancy
-from payments.services import PaymentCreateService
-from users.services import user_account_create, user_authenticate
+from apartments import models as a_models, selectors as a_selectors, services as a_services
+from semesters import models as s_models, selectors as s_selectors
+from tenancy import models as t_models, selectors as t_selectors, services as t_services
+from payments import models as p_models, selectors as p_selectors, services as p_services
+from users import services as u_services, models as u_models, selectors as u_selectors
 
 def run():
-    s = semester_current().start_date
-    before = datetime(s.year, s.month, s.day, tzinfo=timezone.utc)
-    print("Starting at: " ,before.date(), before.time())
-    with freeze_time(before, tz_offset=0) as frozen_time:
-        prev_sem = sem_selectors.semester_within_grace_period()
-        print("Current grace period semester:\t", prev_sem, "\n")
-        
-        one_week_later = before + timedelta(weeks=1)
-        frozen_time.move_to(one_week_later)
-        print("Frozen date one week later:", tz.now().date())
-        prev_sem = sem_selectors.semester_within_grace_period()
-        print("Semester:\t" ,prev_sem, "\n")
-        
-        two_weeks_later = before + timedelta(weeks=2)
-        frozen_time.move_to(two_weeks_later)
-        print("Frozen date two weeks later:", tz.now().date())
-        prev_sem = sem_selectors.semester_within_grace_period()
-        print("Semester:\t" ,prev_sem, "\n")
-        
-        two_weeks_one_hour_later = before + timedelta(weeks=2, hours=1)
-        frozen_time.move_to(two_weeks_one_hour_later)
-        print("Frozen date two weeks later:", tz.now().date())
-        prev_sem = sem_selectors.semester_within_grace_period()
-        print("Semester:\t", prev_sem, "\n")
-        
+    user = u_models.User.objects.get(pk=5)
+    semester = s_selectors.semester_current()
+    tenant = t_selectors.tenancy_recent(user)
+    print("Recent apartment for user", tenant.apartment_id)
+    
+    apt_list = a_selectors.apartment_list_for(user=user)
+    rentable = a_models.Apartment.objects.filter(rentable=True)
+    pprint([{"apartment_id": apt.pk, "rentable": apt.rentable} for apt in apt_list])
+    pprint([{"apartment_id": apt.pk, "rentable": apt.rentable} for apt in rentable])
+    
