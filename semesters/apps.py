c301907d4080a225309980476bb3bc69e697b62b
munene25 <edmune25@gmail.com>
2026-05-14 16:21:06 +0300

    semesters: - remove semesters app and related files

--- semesters/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SemestersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'semesters'
--- semesters/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.SemesterListCreateView.as_view(), name="list_create"),
-    path("<int:semester_id>/", views.SemesterDetailUpdateDeleteView.as_view(), name="detail_update"),
-]
