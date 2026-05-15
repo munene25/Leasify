040b07873c8d3dab912e7735bbcd0957d215ed83
munene25 <edmune25@gmail.com>
2026-05-15 15:25:56 +0300

    tenancy.models: - trying a new model for tenancy based on flexible leases.

--- tenancy/models.py
+++ tenancy/models.py
@@ -1,21 +1,23 @@
+from typing import TYPE_CHECKING
+from datetime import timedelta
 from django.db import models
-from enum import StrEnum
-from apartments.models import Apartment
+from django.utils import timezone
 from users.models import User
 from common.models import BaseModel
-from decimal import Decimal
-from django.utils import timezone
-from datetime import timedelta
+from apartments.models import Apartment
+
+if TYPE_CHECKING:
+    from payments.models import Payment
+    from django.db.models import QuerySet
+
+
 
 GRACE_PERIOD = timedelta(weeks=2)
+DEFAULT_RESERVATION_DURATION = timedelta(days=2)
+MOVING_WINDOW = timedelta(weeks=2)
+MAX_RESERVATIONS_PER_USER = 2
 
-
-class TenantPaymentStatus(StrEnum):
-    UNPAID = "unpaid"
-    PENDING = "pending"
-    CLEARED = "cleared"
-    OVERPAID = "overpaid"
-
+get_default_reservation_expiry_date = lambda: timezone.now().date() + DEFAULT_RESERVATION_DURATION
 
 class Tenancy(BaseModel):
     """
@@ -33,32 +35,28 @@ class Tenancy(BaseModel):
             models.Index(fields=["apartment", "start_date", "end_date"]),
         ]
 
+    class TenancyStatus(models.TextChoices):
+        PENDING = "pending", "Tenant has made a reservation but has not paid yet"
+        ACTIVE = "active", "Tenant has paid and the tenancy is active"
+        EXPIRED = "expired", "Reservation has expired without payment"
+        TERMINATED = "terminated", "Tenancy has been explicitly terminated"
+
+
     user = models.ForeignKey(User, on_delete=models.PROTECT, null=False, blank=False)
     apartment = models.ForeignKey(Apartment, on_delete=models.PROTECT, null=False, blank=False)
     start_date = models.DateField(null=False, blank=False)
     end_date = models.DateField(null=False, blank=False)
-    lease_rent = models.DecimalField(decimal_places=2, max_digits=10)
+    total_due = models.DecimalField(decimal_places=2, max_digits=10)
     total_paid = models.DecimalField(decimal_places=2, max_digits=10)
+    status = models.CharField(max_length=20, choices=TenancyStatus.choices, default=TenancyStatus.PENDING)
+    reservation_expiration = models.DateField(null=False, blank=False, default=get_default_reservation_expiry_date)
+
     user_id: int
     apartment_id: int
+    payment_records: "QuerySet[Payment]"
 
     def __str__(self) -> str:
         return f"user:{self.user_id} - tenancy:{self.pk}"
-
-    @property
-    def balance(self) -> Decimal:
-        """The amount of money owed by the tenant"""
-        return self.lease_rent - self.total_paid
-
-    @property
-    def payment_status(self) -> TenantPaymentStatus:
-        if self.total_paid <= 0:
-            return TenantPaymentStatus.UNPAID
-        elif self.total_paid < self.lease_rent:
-            return TenantPaymentStatus.PENDING
-        elif self.total_paid == self.lease_rent:
-            return TenantPaymentStatus.CLEARED
-        return TenantPaymentStatus.OVERPAID
 
     @property
     def active(self) -> bool:
@@ -67,16 +65,18 @@ class Tenancy(BaseModel):
         return self.start_date <= now <= self.end_date
 
     @property
+    def duration_months(self,) -> int:
+        """
+        Calculate the duration of the tenancy in months
+        This calculation relies on the fact that all tenancies begin on the first day of the month and last day of the month.
+        Enforced at the service layer when creating tenancies.
+        """
+
+        from dateutil.relativedelta import relativedelta
+
+        return relativedelta(self.end_date, self.start_date).months + 1
+        
+    @property
     def has_ended(self) -> bool:
         """Check if the tenancy has ended"""
         return self.end_date < timezone.now().date()
-
-    def duration_days(self) -> int:
-        return (self.end_date - self.start_date).days
-    
-    def duration_months(self) -> int:
-        return self.duration_days() // 30  # Approximate month as 30 days
-    
-    def effective_rent(self) -> Decimal:
-        """calculate the total rent for the tenancy period"""
-        return (self.lease_rent * self.duration_months()).quantize(Decimal("0.01"))
