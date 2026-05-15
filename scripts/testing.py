de09af5a5d7707153f5ca7425a6742ba9924f585
munene25 <edmune25@gmail.com>
2026-05-15 16:58:53 +0300

    scripts.testing: - removed scripts

--- scripts/testing.py
+++ scripts/testing.py
@@ -1,5 +1,5 @@
 from freezegun import freeze_time
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta, timezone, date
 from pprint import pprint
 from django.db import models, connection
 from django.utils import timezone as tz
@@ -7,19 +7,9 @@ from django.contrib.auth.models import Permission
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from apartments import models as a_models, selectors as a_selectors, services as a_services
-from semesters import models as s_models, selectors as s_selectors
 from tenancy import models as t_models, selectors as t_selectors, services as t_services
 from payments import models as p_models, selectors as p_selectors, services as p_services
 from users import services as u_services, models as u_models, selectors as u_selectors
 
 def run():
-    user = u_models.User.objects.get(pk=5)
-    semester = s_selectors.semester_current()
-    tenant = t_selectors.tenancy_recent(user)
-    print("Recent apartment for user", tenant.apartment_id)
-    
-    apt_list = a_selectors.apartment_list_for(user=user)
-    rentable = a_models.Apartment.objects.filter(rentable=True)
-    pprint([{"apartment_id": apt.pk, "rentable": apt.rentable} for apt in apt_list])
-    pprint([{"apartment_id": apt.pk, "rentable": apt.rentable} for apt in rentable])
-    
+    ...
