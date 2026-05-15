f079353dcd94e148eb2032b83f42bc375b112c87
munene25 <edmune25@gmail.com>
2026-05-15 15:31:02 +0300

    tenancy.serializers: - updated serializers to match the service fields

--- tenancy/serializer.py
+++ tenancy/serializer.py
@@ -1,24 +1,23 @@
 from rest_framework import serializers
-
 from common.fields import PhoneNumberSerializerField
 from users.models import User
 from apartments.models import Apartment
-from semesters.models import Semester
+from tenancy.models import Tenancy
 
-# --- Shifted to primary key related fields to centralize object existence validation in serializers ---
-# --- This is necessary only for input serializers ---
 
 class TenancyListSerializer(serializers.Serializer):
     tenant_id = serializers.IntegerField(source="pk")
     tenant_name = serializers.CharField()
     apartment = serializers.CharField()
     payment_status = serializers.CharField()
-    semester = serializers.CharField(source="semester.name")
+    start_date = serializers.DateField()
+    end_date = serializers.DateField()
 
 class TenancyCreateSerializer(serializers.Serializer):
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
-    semester = serializers.PrimaryKeyRelatedField(queryset=Semester.objects.all())
     apartment = serializers.PrimaryKeyRelatedField(queryset=Apartment.objects.all())
+    start_date = serializers.DateField()
+    duration_months = serializers.IntegerField(min_value=1, max_value=4)
 
 class TenancyDetailSerializer(serializers.Serializer):
     tenant_id = serializers.IntegerField(source="pk")
@@ -28,9 +27,18 @@ class TenancyDetailSerializer(serializers.Serializer):
     payment_status = serializers.CharField()
     balance = serializers.DecimalField(max_digits=10, decimal_places=2)
     admission_date = serializers.DateTimeField(source="created_at")
-    semester = serializers.CharField(source="semester.name")
+    start_date = serializers.DateField()
+    end_date = serializers.DateField()
+    lease_rent = serializers.DecimalField(max_digits=10, decimal_places=2)
 
 class TenancyUpdateSerializer(serializers.Serializer):
-    """I think its better to leave semester editing out of the tenancy once created."""
-    # semester = serializers.PrimaryKeyRelatedField(queryset=Semester.objects.all())
+    extension_months = serializers.IntegerField(min_value=1, max_value=4)
     apartment = serializers.PrimaryKeyRelatedField(queryset=Apartment.objects.all())
+
+class TenancyExtendReservationSerializer(serializers.Serializer):
+    reservation_extension = serializers.DurationField()
+    tenancy = serializers.PrimaryKeyRelatedField(queryset=Tenancy.objects.all())
+
+class TenancyTerminateSerializer(serializers.Serializer):
+    tenancy = serializers.PrimaryKeyRelatedField(queryset=Tenancy.objects.all())
+    termination_date = serializers.DateField(queryset=Apartment.objects.all())
