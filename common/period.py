ad1eb97f2de6043511a878f9c9fe16994379aeeb
munene25 <edmune25@gmail.com>
2026-05-19 10:36:03 +0300

    common.period: - Refactored for a much cleaner DateRange. Added a PartialDateRange for filters. - DateRange is frozen to prevent overriding. - Normalization occurs in post_init for assurances.

--- common/period.py
+++ common/period.py
@@ -5,54 +5,54 @@ from calendar import monthrange
 from typing import Self
 
 
-@dataclass
+def normalize_dates(*, start: date, end: date) -> tuple[date, date]:
+    """Normalize start and end dates to 1st of the month and last day of the month"""
+    start_date = start.replace(day=1)
+    last_day_of_end_month = monthrange(end.year, end.month)[1]
+    end_date = end.replace(day=last_day_of_end_month)
+    return start_date, end_date
+
+
+@dataclass(frozen=True)
 class DateRange:
     start_date: date
     end_date: date
     duration_months: int = field(init=False)
 
-    def __post_init__(self):
-        print(f"START: {self.start_date} END: {self.end_date}")
+    def __post_init__(self) -> None:
+        """Calculate the duration in months of the date ranges"""
+        
+        s, e = normalize_dates(start=self.start_date, end=self.end_date)
+        object.__setattr__(self, "start_date", s)
+        object.__setattr__(self, "end_date", e)
+
         if self.start_date >= self.end_date:
             raise ValueError("start date can only occur before the end date")
-        self.duration_months = relativedelta(self.start_date, self.end_date).months + 1
+        object.__setattr__(self, "duration_months", relativedelta(s, e).months + 1)
         
-
-
-    def normalize_dates(self) -> None:
-        """Normalize start and end dates to 1st of the month and last day of the month"""
-        self.start_date.replace(day=1)
-        last_day_of_end_month = monthrange(self.end_date.year, self.end_date.month)[1]
-        self.end_date.replace(day=last_day_of_end_month)
 
     def shift_months(self, start_delta: int, end_delta: int) -> "DateRange":
         """Shift the start_month or end_month by delta"""
-        shift_start = relativedelta(months=start_delta)
-        shift_end = relativedelta(months=end_delta)
 
-        start_date = self.start_date + shift_start
-        end_date = self.end_date + shift_end
-        date_range = DateRange(start_date, end_date)
-        date_range.normalize_dates()
-        return date_range
+        s = self.start_date + relativedelta(months=start_delta)
+        e = self.end_date + relativedelta(months=end_delta)
+        return DateRange(s, e)
 
     @classmethod
     def for_month(cls, date: date) -> "DateRange":
         """Get the date ranges for the month"""
 
-        start_date = date.replace(day=1)
-        last_day_of_end_month = monthrange(start_date.year, start_date.month)[1]
-        end_date = date.replace(day=last_day_of_end_month)
-        return cls(start_date, end_date)
+        s, e = normalize_dates(start=date, end=date)
+        return cls(s, e)
     
     @classmethod
     def compute_lease_window(cls, start_date: date, duration_months: int) -> "DateRange":
         """Return normalized lease period for the tenancy"""
-        d = duration_months - 1
-        if d < 0:
+
+        if duration_months < 1:
             raise ValueError("Minimum lease period is 1 month")
         date_range = cls.for_month(start_date)
-        return date_range.shift_months(0, d)
+        return date_range.shift_months(0, duration_months-1)
     
 
     @classmethod
@@ -64,7 +64,7 @@ class DateRange:
         month_date_range = cls.for_month(date)
 
         if date.day <= GRACE_PERIOD.days:
-            month_date_range.shift_months(-1, 0)
+            return month_date_range.shift_months(-1, 0)
         
         return month_date_range
         
@@ -81,3 +81,41 @@ class DateRange:
         if self.duration_months > 1:
             raise ValueError("The duration range is not a month")
         return self.shift_months(-1, -1)
+
+    
+@dataclass
+class PartialRange:
+    start: date | None = None
+    end: date | None = None
+
+    @classmethod
+    def from_string(cls, range: str, sep: str = "&") -> "PartialRange":
+        """
+        Parse a period range from string into dates.
+        Periods are defined by start and end and accepts either.
+        
+        :param range: the range represented in string format e.g. "start=2000-MAY,end=2001-JUN"
+        :param sep: the seperator used to seperate the two date ranges
+        """
+
+        from calendar import monthrange
+        #Example range: start=2000-MAY,end=2001-JUN
+
+        MONTHS = {
+            "JAN": 1, "FEB": 2, "MAR": 3, "APR": 4,
+            "MAY": 5, "JUN": 6, "JUL": 7, "AUG": 8,
+            "SEP": 9, "OCT": 10, "NOV": 11, "DEC": 12
+        }
+
+        r = cls()
+        
+        ranges = range.split(sep, maxsplit=1)
+
+        for s in ranges:
+            edge, year_month = s.split("=")
+            year_str, month_str = year_month.split("-")
+            year = int(year_str)
+            month = MONTHS[month_str]
+            day = 1 if edge == "start" else monthrange(year, month)[1]
+            setattr(r, edge, date(year, month, day))
+        return r
