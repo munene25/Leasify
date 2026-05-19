7b8c17bbe22aaf3f2bb4940ba3704e198a2c6d1b
munene25 <edmune25@gmail.com>
2026-05-19 17:12:56 +0300

    tests.apartments.test_selectors: - Have to test selectors independently for apartment listings.

---
+++ tests/apartments/test_selectors.py
@@ -0,0 +1,33 @@
+from apartments.selectors import *
+from tenancy.services import tenancy_create
+from apartments.models import Apartment
+from tenancy.models import Tenancy
+from common.period import DateRange
+from django.utils import timezone
+
+
+today = lambda: timezone.now().date()
+
+class TestApartmentList:
+
+    def test_apartment_for_regular_user(self, user_factory, apartment_factory, ):
+        """Test how Policy filters based on user"""
+        apartments = apartment_factory(5)
+        users = user_factory(2)
+
+        list_for_user1 = apartment_list_for(user=users[0], filters={})
+        assert list_for_user1.count() == 5
+
+        list_for_user2 = apartment_list_for(user=users[1], filters={})
+        assert list_for_user2.count() == 5
+
+        # Create a tenancy for user[0] last month and check list filtering again
+        last_month = DateRange.for_month(today()).previous_month()
+        tenancy_create(user=users[0], apartment=apartments[0], start_date=last_month.start_date, duration_months=2)
+        
+        
+        # assert False
+        list_2_for_user1 = apartment_list_for(user=users[0], filters={})
+        assert list_2_for_user1.count() == 5
+        list_2_for_user2 = apartment_list_for(user=users[1], filters={})
+        assert list_2_for_user2.count() == 4
