f0c64d41eeafc6254128211c4dfb0e6bfe03cb5f
munene25 <edmune25@gmail.com>
2026-05-20 17:45:04 +0300

    tenancy.validators: - Now we only have to validate when the reservation is being made and max number of validations. - we need to work on how extending reservations will work

--- tenancy/validators.py
+++ tenancy/validators.py
@@ -1,62 +1,53 @@
-from datetime import timedelta
+from datetime import timedelta, date
 from rest_framework.exceptions import ValidationError
-from common.exceptions import ApartmentOccupiedError, InvalidPeriodError, MaxReservationsExceededError
-from tenancy.models import Tenancy, GRACE_PERIOD, MAX_RESERVATIONS_PER_USER
+from common.exceptions import  MaxReservationsExceededError
+from tenancy.models import Tenancy, MAX_RESERVATIONS_PER_USER
 from common.period import DateRange, today
 
-def validate_lease_period(*, apartment_id: int, date_range: DateRange, exclude_tenancy_id: int | None = None) -> None:
-    """
-    Validate that the lease period is valid (start date before end date and not in the past).
-    As well as no overlapping tenancy exists at the time.
-    ? might consider the extended end date (end_date + grace_time)
-    ? The caller is responsible for determining what date range is passed. Either with grace period or not
-
-    :param apartment_id: The apartment to check for overlapping leases. better to pass it as id rather than apartment.
-    :param start_date: Lease start date
-    :param end_date: Lease end date
-    :raises InvalidPeriod: If the lease period is invalid
-    """
-    # ! Without this, past tenancies can be created.
-    # if date_range.end_date < today():
-    #     raise InvalidPeriodError()
     
-    
-    overlapping = Tenancy.objects.filter(apartment_id=apartment_id, start_date__lt=date_range.end_date, end_date__gt=date_range.start_date)
-
-    if exclude_tenancy_id:
-        overlapping = overlapping.exclude(pk=exclude_tenancy_id)
-
-    if overlapping.exists():
-        raise ApartmentOccupiedError()
-    
-def validate_max_number_of_reservations(user_id: int) -> None:
+def validate_max_monthly_reservations(user_id: int, exceed: int = MAX_RESERVATIONS_PER_USER) -> None:
     """
-    Verify that the number of reservations for the apartment in the past 2 weeks has not exceeded the maximum allowed.
+    We need to protect the system from abuse of tenants spamming bookings.
 
-    :param apartment: The apartment to check
+    :param user_id: user to check against
     :raises ValidationError: If the maximum number of reservations has been exceeded.
-    """
-
-    from django.db.models import Q
-
-    # Check max number of reservations in the past 2 weeks has not reached for the apartment before creating the tenancy record.
-    now = today()
-    start = now - GRACE_PERIOD
-
+    """    
+    r = DateRange.for_month()
     recent_reservations = Tenancy.objects.filter(
         user_id=user_id,
-        reservation_expiration__gte=start,
-        reservation_expiration__lte=now,
-        status__in=(Tenancy.Status.PENDING , Tenancy.Status.EXPIRED),
+        created_at__gte = r.start_date,
+        created_at__lte = r.end_date,
+        status__in=(Tenancy.Status.PENDING , Tenancy.Status.TERMINATED),
     ).count()
-    if recent_reservations > MAX_RESERVATIONS_PER_USER:
+    if recent_reservations > exceed:
         raise MaxReservationsExceededError()
 
+
+def validate_lease_period(start_date_raw: date) -> None:
+    """
+    This prevents abuse of new tenants being able to lock an apartment from listings
+    by setting dates in the future
+    """
+    now = today()
+    target_month = DateRange.for_month(start_date_raw)
+    this_month = DateRange.for_month(now)
+    next_month = this_month.next_month()
+
+    # At any point in time, you can only create tenancies for this or next month
+    if target_month not in (this_month, next_month):
+        raise ValidationError("Cannot create booking for this time")
+
+    # Filter out bookings made before 21st for next month    
+    if target_month == next_month and now.day < 21:
+        raise ValidationError("Bookings for next month open on the 21st.")
+    
+
+
 def validate_reservation_extension(tenancy: Tenancy, reservation_extension: timedelta) -> None:
-    computed_reservation_expiration = tenancy.reservation_expiration + reservation_extension
+    computed_reservation_expiration = tenancy.reservation_expriry + reservation_extension
     if tenancy.status != Tenancy.Status.PENDING:
         raise ValidationError("Only pending tenancies can have their reservations extended.")
     if reservation_extension > timedelta(days=21):
         raise ValidationError("Reservation extension cannot be longer than 3 weeks.")
-    if computed_reservation_expiration >= tenancy.end_date:
-        raise ValidationError("Reservation extension cannot extend beyond the lease end date.")
+    # if computed_reservation_expiration >= tenancy.end_date:
+    #     raise ValidationError("Reservation extension cannot extend beyond the lease end date.")
