064dde8273f80630bc296729266afef5a25a0466
munene25 <edmune25@gmail.com>
2026-05-21 11:52:16 +0300

    tenancy.selectors: - TenancyFilterPolicy is more clearly defined. - Removed previous filtering filtersets. - Added tenancy locking. This might be useful especially since we'll have round the clock agents working on tenancy objects.

--- tenancy/selectors.py
+++ tenancy/selectors.py
@@ -1,12 +1,10 @@
 from typing import Any, TYPE_CHECKING
 from django.http.request import QueryDict
-from django.db.models import Q, F, QuerySet, Prefetch
-from rest_framework.exceptions import ValidationError
+from django.db.models import Q, QuerySet, Prefetch
 from tenancy.models import Tenancy
 from common.helpers import raise_not_found
 from common.domain import FilteringPolicy
-from django.utils import timezone
-from common.period import DateRange
+from tenancy.choices import *
 
 if TYPE_CHECKING:
     from users.models import User
@@ -15,7 +13,6 @@ BASE_QS: QuerySet[Tenancy] = Tenancy.objects.select_related("user", "apartment")
 
 tenancy_not_found = raise_not_found("tenancy_id", "Tenancy does not exist.")
 
-occupied = [Tenancy.Status.ACTIVE, Tenancy.Status.PENDING]
 
 class TenancyFilterPolicy(FilteringPolicy):
     """
@@ -23,8 +20,11 @@ class TenancyFilterPolicy(FilteringPolicy):
     Other users should be able to view tenancies belonging to them.
     """
 
-    SUPERUSER = MANAGER = CARETAKER = Q()
-    TENANT = REGULAR = lambda user: Q(user_id=user.pk)
+    SUPERUSER = Q()
+    MANAGER = Q()
+    CARETAKER = Q()
+    TENANT = lambda user: Q(user_id=user.pk)
+    REGULAR = lambda user: Q(user_id=user.pk)
 
 
 def tenancy_list_for(user: "User", filters: QueryDict | dict[str, Any]) -> QuerySet[Tenancy]:
@@ -50,23 +50,9 @@ def tenancy_list_for(user: "User", filters: QueryDict | dict[str, Any]) -> Query
                 q |= Q(apartment__block__icontains=value)
                 q |= Q(user__first_name__icontains=value)
                 q |= Q(user__last_name__icontains=value)
+                q |= Q(status__icontains=value)
             return queryset.filter(q)
 
-        def is_cleared(self, queryset, name, value: bool):
-            f = F("apartment__rent")
-            q = Q(total_paid__gte=f) if value else Q(total_paid__lt=f)
-            return queryset.filter(q)
-
-        def filter_period(self, queryset, name, value: str):
-            """Example filters: period=from=2000-MAY,to=2001-JUN"""
-            from common.period import PartialRange
-            
-            try:
-                r = PartialRange.from_string(value)
-            except (ValueError, KeyError):
-                raise ValidationError("Cannot parse date ranges. Format is 'period=start=YYYY-MMM,end=YYYY-MMM'")
-
-            return queryset.filter(end_date__gte=r.start, start_date__lte=r.end)
 
     t_filters = TenancyFilterPolicy.for_user(user)
     tenancies = BASE_QS.select_related("semester").filter(t_filters)
@@ -80,14 +66,16 @@ def tenancy_get_for(user: "User", tenancy_id: int) -> Tenancy:
     t_filters = TenancyFilterPolicy.for_user(user)
     return BASE_QS.filter(t_filters).get(pk=tenancy_id)
 
+@tenancy_not_found
+def tenancy_lock(tenancy_id: int) -> Tenancy:
+    """Lock the current tenancy"""
+    return Tenancy.objects.select_for_update().get(pk=tenancy_id)
+
 
 @tenancy_not_found
 def tenancy_get(tenancy_id: int) -> Tenancy:
     """Get tenant outside of client flows"""
-
     return BASE_QS.get(pk=tenancy_id)
 
-
-
 def current_tenant() -> Prefetch:
-    return Prefetch("tenancy_set", Tenancy.objects.filter(status__in=[Tenancy.Status.ACTIVE]) , to_attr="_active_tenants")
+    return Prefetch("tenancy_set", Tenancy.objects.filter(status__in=active_reserved_defaulting) , to_attr="_active_tenant")
