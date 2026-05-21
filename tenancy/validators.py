2b5016426965dc8f59aa3b4b5b48c43fa3b3ea35
munene25 <edmune25@gmail.com>
2026-05-21 13:26:12 +0300

    tenancy.validators: - renamed pending to RESERVED

--- tenancy/validators.py
+++ tenancy/validators.py
@@ -2,6 +2,7 @@ from datetime import timedelta, date
 from rest_framework.exceptions import ValidationError
 from common.exceptions import  MaxReservationsExceededError
 from tenancy.models import Tenancy, MAX_RESERVATIONS_PER_USER
+from tenancy.choices import Status
 from common.period import DateRange, today
 
     
@@ -17,7 +18,7 @@ def validate_max_monthly_reservations(user_id: int, exceed: int = MAX_RESERVATIO
         user_id=user_id,
         created_at__gte = r.start_date,
         created_at__lte = r.end_date,
-        status__in=(Tenancy.Status.PENDING , Tenancy.Status.TERMINATED),
+        status__in=(Status.RESERVED , Status.TERMINATED),
     ).count()
     if recent_reservations > exceed:
         raise MaxReservationsExceededError()
@@ -45,8 +46,8 @@ def validate_lease_period(start_date_raw: date) -> None:
 
 def validate_reservation_extension(tenancy: Tenancy, reservation_extension: timedelta) -> None:
     computed_reservation_expiration = tenancy.reservation_expriry + reservation_extension
-    if tenancy.status != Tenancy.Status.PENDING:
-        raise ValidationError("Only pending tenancies can have their reservations extended.")
+    if tenancy.status != Status.RESERVED:
+        raise ValidationError("Only reserved tenancies can have their reservations extended.")
     if reservation_extension > timedelta(days=21):
         raise ValidationError("Reservation extension cannot be longer than 3 weeks.")
     # if computed_reservation_expiration >= tenancy.end_date:
