d4f8a8c595badb582e4f9bec239dc23648974ba4
munene25 <edmune25@gmail.com>
2026-05-21 14:08:36 +0300

    tenancy.services: - added active to defaulting and reserved to terminated

--- tenancy/services.py
+++ tenancy/services.py
@@ -1,6 +1,5 @@
 from typing import TYPE_CHECKING
 from structlog import get_logger
-
 from django.db import transaction
 from django.contrib.auth.models import Group
 from rest_framework.exceptions import ValidationError
@@ -8,11 +7,11 @@ from common.period import DateRange
 from users.services import user_set_role
 from tenancy.validators import validate_max_monthly_reservations, validate_lease_period
 from tenancy.models import Tenancy
-from tenancy.choices import Status
-from tenancy.selectors import tenancy_lock
+from tenancy.choices import Status, TerminationReason
+from tenancy.selectors import tenancy_lock, tenancy_in
 from apartments.selectors import apartment_lock
 from billing.services import billing_period_increment, billing_period_initialize
-
+from billing.choices import BillingStatus
 
 logger = get_logger("tenancy.services")
 
@@ -20,12 +19,13 @@ if TYPE_CHECKING:
     from users.models import User
     from datetime import date
     from apartments.models import Apartment
+    from billing.models import BillingPeriod
 
 
 @transaction.atomic()
 def tenancy_create(*, user: "User", apartment: "Apartment", start_date: "date", duration_months: int) -> Tenancy:
     """
-    
+
     Create a tenancy as well as a billing period the selected duration
     Check if dates are valid for creation
     Add user to tenant group.
@@ -38,7 +38,7 @@ def tenancy_create(*, user: "User", apartment: "Apartment", start_date: "date", 
     :param total_due: The total amount due for the tenancy. If not provided, it will be calculated based on the apartment rent and lease duration.
     :return: The created tenancy record.
     """
-    
+
     # Users can only make a certain number of reservations in a month.
     validate_max_monthly_reservations(user.pk)
 
@@ -58,7 +58,7 @@ def tenancy_create(*, user: "User", apartment: "Apartment", start_date: "date", 
     # Add user to Tenant group if not already
     if not user.groups.filter(name="tenant").exists():
         user_set_role(user=user, role=Group.objects.get(name="tenant"))
-    
+
     logger.info(
         "tenancy_created",
         tenancy_id=t.pk,
@@ -70,17 +70,62 @@ def tenancy_create(*, user: "User", apartment: "Apartment", start_date: "date", 
     billing_period_initialize(tenancy=t, date_r=r)
     return t
 
-def tenancy_renew_lease(tenancy: Tenancy, duration_months: int):
-    """A tenant wishes to create a new billing period for themselves"""    
+
+def tenancy_renew_lease(tenancy: Tenancy, duration_months: int) -> "BillingPeriod":
+    """A tenant wishes to create a new billing period for themselves"""
     from billing.choices import BillingStatus
 
-    # Tenant should have an existing billing history and either 
+    # Tenant should have an existing billing history and either
     if not tenancy.is_continuing or not tenancy.last_billing:
         raise ValidationError("Only continuing tenancies can create a tenancy")
-    
+
     if tenancy.last_billing.status == BillingStatus.UNPAID:
         raise ValidationError("You have an pending billing period, cancel it to create a new one")
-        
+
     tenancy = tenancy_lock(tenancy.pk)
 
-    billing_period_increment(tenancy=tenancy, duration_months=duration_months)
+    return billing_period_increment(tenancy=tenancy, duration_months=duration_months)
+
+
+
+def tenancy_reserved_to_terminated() -> None:
+    """
+    Fetch all tenancies that are reserved, then filter the ones whose expiration date is past due.
+    Terminate then update termination date and reason.
+    """
+    from common.period import today
+
+    now = today()
+
+    t = tenancy_in([Status.RESERVED]).filter(
+        reservation_expiration__gt=now,
+    )
+
+    t.update(
+        status=Status.TERMINATED,
+        termination_date=now,
+        termination_reason=TerminationReason.EXPIRED,
+    )
+
+def tenancy_active_to_defaulting() -> None:
+    """
+    Fetch all tenancies that are active.
+    Filter the ones that have no paid billing period and terminate them.
+    Update termination date and reason
+    ? Date as param, then task does this?
+    """
+    from common.period import today
+
+    now = today()
+
+    t = tenancy_in([Status.ACTIVE]).exclude(
+        billingperiod__start_date__lte=now,
+        billingperiod__end_date__lte=now,
+        billingperiod__status=BillingStatus.PAID,
+    )
+
+    t.update(
+        status=Status.DEFAULTING, 
+        termination_date=now, 
+        termination_reason=TerminationReason.NONPAYMENT
+    )
