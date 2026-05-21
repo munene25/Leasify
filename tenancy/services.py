9748e1ce6773cc1618b630c30765ac299de593c7
munene25 <edmune25@gmail.com>
2026-05-21 13:25:20 +0300

    tenancy.services: - reworked how tenancy renewal is handled

--- tenancy/services.py
+++ tenancy/services.py
@@ -1,24 +1,29 @@
 from typing import TYPE_CHECKING
 from structlog import get_logger
-from datetime import date
+
 from django.db import transaction
-from tenancy.models import Tenancy, GRACE_PERIOD
-from tenancy.validators import validate_max_monthly_reservations, validate_lease_period
 from django.contrib.auth.models import Group
-from users.services import user_set_role
-from apartments.selectors import apartment_lock
-from billing.services import billing_period_initialize
+from rest_framework.exceptions import ValidationError
 from common.period import DateRange
+from users.services import user_set_role
+from tenancy.validators import validate_max_monthly_reservations, validate_lease_period
+from tenancy.models import Tenancy
+from tenancy.choices import Status
+from tenancy.selectors import tenancy_lock
+from apartments.selectors import apartment_lock
+from billing.services import billing_period_increment, billing_period_initialize
+
 
 logger = get_logger("tenancy.services")
 
 if TYPE_CHECKING:
     from users.models import User
+    from datetime import date
     from apartments.models import Apartment
 
 
 @transaction.atomic()
-def tenancy_create(*, user: "User", apartment: "Apartment", start_date: date, duration_months: int) -> Tenancy:
+def tenancy_create(*, user: "User", apartment: "Apartment", start_date: "date", duration_months: int) -> Tenancy:
     """
     
     Create a tenancy as well as a billing period the selected duration
@@ -33,20 +38,24 @@ def tenancy_create(*, user: "User", apartment: "Apartment", start_date: date, du
     :param total_due: The total amount due for the tenancy. If not provided, it will be calculated based on the apartment rent and lease duration.
     :return: The created tenancy record.
     """
+    
+    # Users can only make a certain number of reservations in a month.
     validate_max_monthly_reservations(user.pk)
-    validate_lease_period(start_date)
 
+    # Even though the lease period is tied to the billing period.
+    # I need to check that they are not booking too far in advance
+    validate_lease_period(start_date)
 
     apt = apartment_lock(apartment.pk)
     t = Tenancy(
         user=user,
         apartment=apt,
-        status=Tenancy.Status.PENDING,
+        status=Status.RESERVED,
     )
     t.full_clean()
     t.save()
 
-    #Add user to Tenant group if not already
+    # Add user to Tenant group if not already
     if not user.groups.filter(name="tenant").exists():
         user_set_role(user=user, role=Group.objects.get(name="tenant"))
     
@@ -60,3 +69,18 @@ def tenancy_create(*, user: "User", apartment: "Apartment", start_date: date, du
     r = DateRange.compute_lease_window(start_date, duration_months)
     billing_period_initialize(tenancy=t, date_r=r)
     return t
+
+def tenancy_renew_lease(tenancy: Tenancy, duration_months: int):
+    """A tenant wishes to create a new billing period for themselves"""    
+    from billing.choices import BillingStatus
+
+    # Tenant should have an existing billing history and either 
+    if not tenancy.is_continuing or not tenancy.last_billing:
+        raise ValidationError("Only continuing tenancies can create a tenancy")
+    
+    if tenancy.last_billing.status == BillingStatus.UNPAID:
+        raise ValidationError("You have an pending billing period, cancel it to create a new one")
+        
+    tenancy = tenancy_lock(tenancy.pk)
+
+    billing_period_increment(tenancy=tenancy, duration_months=duration_months)
