541f414f6b92133875eec90deb59f24c2aee4d0d
munene25 <edmune25@gmail.com>
2026-05-21 15:34:37 +0300

    billing.services: - added status changing service for billing period

--- billing/services.py
+++ billing/services.py
@@ -1,12 +1,15 @@
 from typing import TYPE_CHECKING
+from decimal import Decimal
 from structlog import get_logger
 from django.db import transaction
 from rest_framework.exceptions import ValidationError
 from common.period import DateRange
 from billing.models import BillingPeriod, MAX_BILLING_PERIOD
 from billing.selectors import billing_last_paid_for
+from billing.choices import BillingStatus
 
 if TYPE_CHECKING:
+    from payments.models import Payment
     from tenancy.models import Tenancy
 
 logger = get_logger("billing.services")
@@ -21,11 +24,13 @@ def billing_period_initialize(*, tenancy: "Tenancy", date_r: DateRange) -> Billi
     if date_r.duration_months > MAX_BILLING_PERIOD:
         raise ValidationError("Max Billng period exceeded")
     
+    total_due = (tenancy.apartment.rent * date_r.duration_months).quantize(Decimal("0.01"))
+
     bp = BillingPeriod(
         tenancy=tenancy,
         start_date=date_r.start_date,
         end_date=date_r.end_date,
-        rent_snapshot=tenancy.apartment.rent,
+        rent_snapshot=total_due,
     )
     bp.full_clean()
     bp.save()
@@ -58,12 +63,12 @@ def billing_period_increment(*, tenancy: "Tenancy", duration_months: int) -> Bil
   
     
     r = DateRange.compute_lease_window(last_billing.next_start, duration_months)
-    
+    total_due = (tenancy.apartment.rent * r.duration_months).quantize(Decimal("0.01"))
     bp = BillingPeriod(
         tenant=tenancy,
         start_date=r.start_date,
         end_date=r.end_date,
-        rent_snapshot=tenancy.apartment.rent,
+        rent_snapshot=total_due,
     )
     bp.full_clean()
     bp.save()
@@ -75,3 +80,30 @@ def billing_period_increment(*, tenancy: "Tenancy", duration_months: int) -> Bil
 
     )
     return bp
+
+@transaction.atomic
+def billing_period_update(billing: BillingPeriod, status: BillingStatus, payment: "Payment | None") -> BillingPeriod:
+    
+
+    locked = BillingPeriod.objects.select_for_update().get(billing.pk)
+
+    if locked.status == BillingStatus.PAID:
+        raise ValidationError("Cannot complete request. Billing period cannot be changed once created")
+    
+    if status == BillingStatus.UNPAID:
+        raise ValidationError("Operation cannot be completed")
+    
+    if status == BillingStatus.CANCELED:
+        locked.status = status
+        locked.save(update_fields=["status"])
+        return locked
+    
+    if not payment or payment.status != "confirmed":
+        raise ValidationError("Required confirmed payment instance to complete request")
+    
+    if payment.amount < billing.total_due:
+        raise ValidationError("Amount not enough")
+
+    locked.status = status
+    locked.save(update_fields=["status"])
+    return locked
