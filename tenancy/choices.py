3b6420d41a625acc386f749b6ae2a2ad39ce4924
munene25 <edmune25@gmail.com>
2026-05-21 11:41:42 +0300

    tenancy.choices: - moved tenancy model text choices to avoid importing from the Tenancy model directly.

---
+++ tenancy/choices.py
@@ -0,0 +1,23 @@
+""""
+
+This seperation is intended to avoid model not ready
+Also from time to time I end up dealing with circular imports.
+
+"""
+from django.db.models import TextChoices
+
+
+class Status(TextChoices):
+    ACTIVE = "active", "Tenant has paid and the tenancy is active"
+    RESERVED = "pending", "Tenant has made a reservation but has not paid yet"
+    DEFAULTING = "defaulting", "Active tenant has not yet paid"
+    TERMINATED = "terminated", "Tenancy has been expired"
+
+class TerminationReason(TextChoices):
+    MANAGERIAL = "managerial", "Managerial decision to terminate"
+    VOLUNTARY = "voluntary", "Voluntary termination"
+    NONPAYMENT = "non_payment", "Tenant failed to pay"
+
+
+active_reserved_defaulting = [Status.ACTIVE, Status.RESERVED, Status.DEFAULTING]
+reserved_or_terminated = [Status.RESERVED, Status.TERMINATED]
