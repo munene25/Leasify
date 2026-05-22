30253b71fc3a469e872da5ee8c2f4fed2bf9c355
munene25 <edmune25@gmail.com>
2026-05-22 19:59:25 +0300

    payments.services: - Huge refactor, removed transaction type. - removed status on instantiation, initiated payments will always be pending.

--- payments/services.py
+++ payments/services.py
@@ -1,96 +1,96 @@
-from typing import Any, TYPE_CHECKING
+from typing import TYPE_CHECKING
 from payments.models import Payment
 from django.db import transaction
 from rest_framework.exceptions import ValidationError
-from payments.choices import TransactionType, PaymentStatus, PaymentInitiator 
-
+from payments.choices import PaymentStatus, PaymentInitiator
 from structlog import get_logger
+from uuid import uuid4
 
 logger = get_logger("payments.services")
 
 if TYPE_CHECKING:
+    from payments.mpesa import CallbackResponse
     from billing.models import BillingPeriod
 
 
-
 @transaction.atomic
-def payment_initiate(billing: "BillingPeriod", transaction_type: TransactionType, status: PaymentStatus, phone_number: str, initiator: PaymentInitiator, stk_push: bool) -> Payment:
+def payment_initiate(
+    billing: "BillingPeriod",
+    phone_number: str,
+    initiator: PaymentInitiator,
+    stk_push: bool = False,
+) -> Payment:
     """
     Initialize a payment. Depending on status can be updated later on callback
-    
+
     :param billing:
     :param transaction_type:
     :param status:
-    :param phone_number: phone number to 
+    :param phone_number: phone number to
     :param initiator: The short code for who is initiating the request. determined upstream
 
     """
+    # ? if initiator is tenant dont allow status confirmed?
     
-    if status == PaymentStatus.FAILED:
-        raise ValidationError("Operation not allowed")
-    
-    #? if initiator is tenant dont allow status confirmed?
+    if stk_push:
+        from payments.mpesa.stk_push import intiate_stk_push
+
+        r = intiate_stk_push(
+            phone_number=phone_number,
+            amount=int(billing.total_due),
+            account_ref=str(billing),
+            description="Rent Payment",
+        )
+        checkout_id = r.checkout_id
+    else:
+        checkout_id = str(uuid4())
 
     payment = Payment(
         billing=billing,
-        transaction_type=transaction_type,
         phone_number=phone_number,
         amount=billing.total_due,
-        status=status,
+        status=PaymentStatus.PENDING,
+        checkout_id=checkout_id,
         initiator=initiator,
+        payee=billing.tenancy.user.full_name
     )
+
     payment.full_clean()
     payment.save()
 
     logger.info(
         "payment_initiated",
         tenancy_id=billing.tenancy_id,
-        billing_period=str(billing),
-        payment_amount=billing.total_due,
-        status=status,
-        phone_number=phone_number,
-        payment_ref=payment.ref_no
+        billing_period=billing.name,
+        amount=billing.total_due,
+        phone_number=payment.phone_number,
+        payment_ref=payment.ref_no,
     )
-    if not stk_push:
-        return payment
-    
-    if transaction_type == TransactionType.DEBIT and initiator == PaymentInitiator.TENANT:
-        # initialize stk...
-        raise NotImplemented("This is where the stk push request is initialized")
-    
-    raise ValidationError("You cannot perform an stk push for such a transaction")
+    return payment
 
 
 @transaction.atomic
-def payment_confirm(payment: Payment, payload: dict[str, Any]) -> Payment:
+def payment_confirm(r: "CallbackResponse") -> Payment:
     """
-    Assuming with a callback, this is where the validation happens and transaction status is changed
-
-    :param payment: The payment instance to modify.
-    :param payload: The payload in which to validate the status.
+    Parse callback response and return an upadted payment.
+    Update the payment status and receipt no if it's successfull
     """
-    # validate payload:
-    locked = Payment.objects.select_for_update().get(pk=payment.pk)
-    if locked.status == PaymentStatus.CONFIRMED:
-        raise ValidationError("Operation not allowed")
+
+    checkout_id = r.checkout_id
+
+    payment = Payment.objects.select_for_update().get(checkout_id=checkout_id)
+
+    if payment.status in [PaymentStatus.FAILED, PaymentStatus.SUCCESS]:
+        raise ValidationError("Operation not allowed, payment already closed")
+
+    payment.status =  PaymentStatus.SUCCESS if r.success else PaymentStatus.FAILED
+
+    if r.success:
+        payment.receipt_no = r.receipt_no
     
-    # mock validation
-    confirmation = payload.get("confirmed", None)
-    
+    # Generate receipt?
 
-    new_status  = PaymentStatus.CONFIRMED if confirmation else PaymentStatus.FAILED
-
-    if new_status == PaymentStatus.FAILED:
-        # ?Could add a means of adding metadata on why
-        pass
-
-    locked.status = new_status
-
-    locked.full_clean()
-    locked.save()
-    logger.info(
-        "payment_confirmed",
-        payment_ref=payment.ref_no
-    )
-    return locked
-        
+    payment.full_clean()
+    payment.save()
+    logger.info("payment_confirmed", checkout_id=payment.checkout_id, status=payment.status)
+    return payment
