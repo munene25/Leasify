0e33d9455b8f927aa898afcf9c1f5c0553276b81
munene25 <edmune25@gmail.com>
2026-05-22 19:48:47 +0300

    payments.models: - added receipt_no and checkout_id field: - removed id uuid field

--- payments/models.py
+++ payments/models.py
@@ -3,14 +3,12 @@ from billing.models import BillingPeriod
 import uuid
 from common.models import BaseModel
 from common.fields import PhoneNumberModelField
-from payments.choices import PaymentStatus, TransactionType, PaymentInitiator
+from payments.choices import PaymentStatus, PaymentInitiator
 
 class Payment(BaseModel):
     """
     Payment will have 2 states, once initiated and then confirmed
-    At inititaion, phone, ref, amount, transaction_type, billing status and intiator are defined
-    At confirmation, the payee is filled out.
-    ? Maybe get the mpesa confirmation code upon
+    On confirmation, status changes to either FAILED or SUCCESS.
     """
     class Meta:
         indexes = [
@@ -19,12 +17,11 @@ class Payment(BaseModel):
 
 
     billing = models.ForeignKey(BillingPeriod, null=False, blank=False, on_delete=models.PROTECT, related_name="payments")
-    id = models.UUIDField(primary_key=True, db_index=True, default=uuid.uuid4, editable=False)
+    receipt_no = models.CharField(null=True, blank=True)
     initiator = models.CharField(null=False, blank=False, choices=PaymentInitiator)
     phone_number = PhoneNumberModelField(null=False, blank=False, editable=False)
+    checkout_id = models.CharField(db_index=True, unique=True, null=False, blank=False)
     amount = models.DecimalField(decimal_places=2, max_digits=10, editable=False, blank=False, null=False)
-    transaction_type = models.CharField(null=False,blank=False, max_length=10, editable=False, choices=TransactionType.choices)
-   
     status = models.CharField(null=False, blank=False, choices=PaymentStatus, default=PaymentStatus.PENDING)
     payee = models.CharField(max_length=30, null=True, blank=True)
 
@@ -32,5 +29,5 @@ class Payment(BaseModel):
 
     @property
     def ref_no(self) -> str:
-        return f"{self.initiator}-{str(self.pk).upper()}"
+        return f"{self.initiator}-{str(self.checkout_id).upper()}"
 
