8b5e04f18acd6833f37321163b26096c500fef6f
munene25 <edmune25@gmail.com>
2026-05-22 19:46:53 +0300

    payments.choices: - No need for transaction types.

--- payments/choices.py
+++ payments/choices.py
@@ -1,9 +1,5 @@
 from django.db.models import TextChoices
 
-class TransactionType(TextChoices):
-    DEBIT = "debit", "Incoming Payments"
-    CREDIT = "credit", "Outgoing Payments"
-
 class PaymentStatus(TextChoices):
     PENDING = "pending", "Payment has been initiated but not confirmed"
     SUCCESS = "success", "Payment has been confirmed and funds have transferred"
