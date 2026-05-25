bacfd9242908d05fcf1d7e8a44cda0688829853c
munene25 <edmune25@gmail.com>
2026-05-25 15:25:08 +0300

    tests.apartments.test_selectors: - tenancy_validators_patch is better?

--- tests/apartments/test_selectors.py
+++ tests/apartments/test_selectors.py
@@ -5,12 +5,12 @@ from tenancy.models import Tenancy
 from common.period import DateRange
 from django.utils import timezone
 
-
 today = lambda: timezone.now().date()
+
 
 class TestApartmentList:
 
-    def test_apartment_for_regular_user(self, user_factory, apartment_factory, patch_tenancy_validators  ):
+    def test_apartment_for_regular_user(self, user_factory, apartment_factory, tenancy_validators_patch):
         """Test how Policy filters based on user"""
         apartments = apartment_factory(5, overrides={"rentable": True})
         users = user_factory(2)
@@ -24,7 +24,7 @@ class TestApartmentList:
         # Create a tenancy for user[0] last month and check list filtering again
         last_month = DateRange.for_month(today()).previous_month()
         t = tenancy_create(user=users[0], apartment=apartments[0], start_date=last_month.start_date, duration_months=2)
-        
+
         # assert False
         list_2_for_user1 = apartment_list_for(user=users[0], filters={})
         assert list_2_for_user1.count() == 4
