10e32032b42eb3b4f4678775f3e5de6bb999750e
munene25 <edmune25@gmail.com>
2026-05-25 15:07:53 +0300

    tests.apartments.test_selectors: - added a patch for tenancy_validators. - all tenants and regular users will now see the same list of listable apartments.

--- tests/apartments/test_selectors.py
+++ tests/apartments/test_selectors.py
@@ -10,9 +10,9 @@ today = lambda: timezone.now().date()
 
 class TestApartmentList:
 
-    def test_apartment_for_regular_user(self, user_factory, apartment_factory, ):
+    def test_apartment_for_regular_user(self, user_factory, apartment_factory, patch_tenancy_validators  ):
         """Test how Policy filters based on user"""
-        apartments = apartment_factory(5)
+        apartments = apartment_factory(5, overrides={"rentable": True})
         users = user_factory(2)
 
         list_for_user1 = apartment_list_for(user=users[0], filters={})
@@ -23,11 +23,10 @@ class TestApartmentList:
 
         # Create a tenancy for user[0] last month and check list filtering again
         last_month = DateRange.for_month(today()).previous_month()
-        tenancy_create(user=users[0], apartment=apartments[0], start_date=last_month.start_date, duration_months=2)
-        
+        t = tenancy_create(user=users[0], apartment=apartments[0], start_date=last_month.start_date, duration_months=2)
         
         # assert False
         list_2_for_user1 = apartment_list_for(user=users[0], filters={})
-        assert list_2_for_user1.count() == 5
+        assert list_2_for_user1.count() == 4
         list_2_for_user2 = apartment_list_for(user=users[1], filters={})
         assert list_2_for_user2.count() == 4
