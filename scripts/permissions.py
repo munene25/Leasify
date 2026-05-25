b8aec949c68ec1182ab3273eb2a3ee5ec8a059fa
munene25 <edmune25@gmail.com>
2026-05-25 14:50:09 +0300

    scripts.permissions: - removed stale permissinons from tenancy model

--- scripts/permissions.py
+++ scripts/permissions.py
@@ -6,13 +6,13 @@ from django.contrib.contenttypes.models import ContentType
 ROLE_PERMISSIONS = {
     "manager": [
         {"apartment" : ["add_apartment", "view_apartment", "change_apartment", "delete_apartment", "view_overview"]},
-        {"tenancy": ["add_tenancy", "view_tenancy", "change_tenancy", "delete_tenancy", "tenancy_extend_lease", "tenancy_extend_reservation_expiration"]},
+        {"tenancy": ["add_tenancy", "view_tenancy", "change_tenancy", "delete_tenancy",]},
         {"user": ["add_user", "view_user", "change_user", "delete_user"]},
         {"payment": ["add_payment", "view_payment"]}
     ],
     "caretaker": [
         {"apartment": ["view_apartment", "change_apartment"]},
-        {"tenancy": ["add_tenancy", "view_tenancy", "change_tenancy", "tenancy_extend_lease"]},
+        {"tenancy": ["add_tenancy", "view_tenancy", "change_tenancy"]},
         {"user": ["add_user", "view_user"]},
         {"payment": ["view_payment"]},
     ],
