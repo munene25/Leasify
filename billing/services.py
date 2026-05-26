81bd4be65630d693cd886d515cbc6eb80896b688
munene25 <edmune25@gmail.com>
2026-05-26 18:07:24 +0300

    billing.services: - removed billing, period increment service. - removed transaction atomic for billing period create.

--- billing/services.py
+++ billing/services.py
@@ -15,8 +15,8 @@ if TYPE_CHECKING:
 logger = get_logger("billing.services")
 
 
-@transaction.atomic
-def billing_period_initialize(*, tenancy: "Tenancy", date_r: DateRange) -> BillingPeriod:
+
+def billing_period_create(*, tenancy: "Tenancy", date_r: DateRange) -> BillingPeriod:
     """
     Can't create a billing period if the previous one is not cleared.
     Duration months cannot exceed 4, this prevents locking rent if rent hikes will be introduced mid period.
@@ -28,52 +28,14 @@ def billing_period_initialize(*, tenancy: "Tenancy", date_r: DateRange) -> Billi
     total_due = (tenancy.apartment.rent * date_r.duration_months).quantize(Decimal("0.01"))
 
     bp = BillingPeriod(
-        tenancy=tenancy,
+        tenancy_id=tenancy.pk,
         start_date=date_r.start_date,
         end_date=date_r.end_date,
         total_due=total_due,
     )
-    bp.full_clean()
     bp.save()
     logger.info(
         "billing_period_created",
-        tenancy_id=tenancy.pk,
-        duration=bp.duration_months,
-        billing_id=bp.pk,
-        billing_period=bp.name,
-    )
-    return bp
-
-
-@transaction.atomic
-def billing_period_increment(*, tenancy: "Tenancy", duration_months: int) -> BillingPeriod:
-    """
-    This is especially for ensuring sequetial billing preiods for ongoing tenants.
-    This will only ever be concerned with the last paid billing period.
-    This ensures the tenancy stay is sequential.
-    """
-    if duration_months > MAX_BILLING_PERIOD:
-        raise ValidationError("Max Billing period exceeded")
-
-    # I need to lock at this point to avoid another billing period to be created at the same time.
-    # There are no db constraints of domain logic
-    last_billing = billing_last_paid_for(tenancy.pk, lock=True)
- 
-    if not last_billing:
-        raise ValidationError("Cannot find a paid billing period")
-
-    r = DateRange.compute_lease_window(last_billing.next_start, duration_months)
-    total_due = (tenancy.apartment.rent * r.duration_months).quantize(Decimal("0.01"))
-    bp = BillingPeriod(
-        tenancy=tenancy,
-        start_date=r.start_date,
-        end_date=r.end_date,
-        total_due=total_due,
-    )
-    bp.full_clean()
-    bp.save()
-    logger.info(
-        "billing_period_renewed",
         tenancy_id=tenancy.pk,
         duration=bp.duration_months,
         billing_id=bp.pk,
