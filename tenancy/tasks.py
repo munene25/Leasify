d4fd3623e02b9f07ee1a9b68edeb180dccd99079
munene25 <edmune25@gmail.com>
2026-05-26 18:22:48 +0300

    tenancy.tasks: - removed stale code

--- tenancy/tasks.py
+++ tenancy/tasks.py
@@ -1,7 +1,3 @@
 from celery import shared_task
 from tenancy import selectors as sl
 
-@shared_task(ignore_results=False)
-def show_detail(tenancy_id):
-    tenancy = sl.tenancy_get(tenancy_id=tenancy_id)
-    return f'User {tenancy.user.get_full_name()} just requested this page'
