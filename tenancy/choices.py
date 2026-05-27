e42afd043a3efb5c97532e2633459555366eabbb
munene25 <edmune25@gmail.com>
2026-05-27 18:36:22 +0300

    tenancy.choices: - added docstring to help while coding

--- tenancy/choices.py
+++ tenancy/choices.py
@@ -9,6 +9,8 @@ from django.db.models import TextChoices
 
 
 class TenancyStatus(TextChoices):
+    "ACTIVE, RESERVED, DEFAULTING, OR TERMINATED"
+
     ACTIVE = "active", "Tenant has paid and the tenancy is active"
     RESERVED = "reserved", "Tenant has made a reservation but has not paid yet"
     DEFAULTING = "defaulting", "Active tenant has not yet paid"
@@ -16,6 +18,8 @@ class TenancyStatus(TextChoices):
 
 
 class TerminationReason(TextChoices):
+    """MANAGERIAL, EXPIRED, VOLUNTARY OR NONPAYMENT"""
+    
     MANAGERIAL = "managerial", "Managerial decision to terminate"
     EXPIRED = "expired", "Reservation expired"
     VOLUNTARY = "voluntary", "Voluntary termination"
