3f467f07f526cb1f9739dab3b297fea4aa3f2d7f
munene25 <edmune25@gmail.com>
2026-05-28 17:36:25 +0300

    config.celery: - reworked how celery beat schedule is set

--- config/celery.py
+++ config/celery.py
@@ -3,23 +3,22 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-# Set the default Django settings module for the 'celery' program.
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = Celery('config')
 
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
-# Load task modules from all registered Django apps.
+
+
 app.autodiscover_tasks()
 
-CELERY_BEAT_SCHEDULE = {
+
+app.conf.beat_schedule = {
     "terminate_reserved": {
-        "task": "tenancy.tasks.terminate_reserved",
-        'schedule': crontab(hour=1, minute=0),  # Every day at 1:00 AM
+        "task": "tenancy.tasks.reserved_set_terminated",
+        'schedule': crontab(minute="*"),  # Every day at 1:00 AM
     },
     "month_start_tasks": {
         "task": "tenancy.tasks.month_start_tasks",
