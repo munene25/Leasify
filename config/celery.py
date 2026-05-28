463e1cd58b84b8d32941b3a64caa15bb5a1063ec
munene25 <edmune25@gmail.com>
2026-05-28 16:46:47 +0300

    config.celery: - scheduling the tasks is added

--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
@@ -14,3 +15,14 @@ app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+CELERY_BEAT_SCHEDULE = {
+    "terminate_reserved": {
+        "task": "tenancy.tasks.terminate_reserved",
+        'schedule': crontab(hour=1, minute=0),  # Every day at 1:00 AM
+    },
+    "month_start_tasks": {
+        "task": "tenancy.tasks.month_start_tasks",
+        "schedule": crontab(hour=0, minute=0, day_of_month=1),  # 1st of month at 12:00 AM
+    },
+}
