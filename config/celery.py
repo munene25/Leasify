ae32b483f4874418b5a5de1333692bc7510b862d
munene25 <edmune25@gmail.com>
2026-05-29 14:27:54 +0300

    config.celery: - Added new task to celery beat

--- config/celery.py
+++ config/celery.py
@@ -11,14 +11,18 @@ app = Celery('config')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-
 app.autodiscover_tasks()
 
-
+# It really is just better to have them run with utc time as most tasks require filtering based on dates.
+# ! Midnight UTC => 3AM (UTC+3)
 app.conf.beat_schedule = {
+    "notify_reserved": {
+        "task": "tenancy.tasks.notify_reserved_on_expiry",
+        'schedule': crontab(hour=0, minute=0),  # Every day at 12:00 AM
+    },
     "terminate_reserved": {
         "task": "tenancy.tasks.reserved_set_terminated",
-        'schedule': crontab(minute="*"),  # Every day at 1:00 AM
+        'schedule': crontab(hour=0, minute=0),  # Every day at 12:00 AM
     },
     "month_start_tasks": {
         "task": "tenancy.tasks.month_start_tasks",
