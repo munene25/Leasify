f24180ac0c0ff80b9530e215edbc4ef8d3fa6a64
munene25 <edmune25@gmail.com>
2026-05-29 14:53:40 +0300

    tenancy.serializer: - reworked serializers to match the new model

--- tenancy/serializer.py
+++ tenancy/serializer.py
@@ -9,9 +9,8 @@ class TenancyListSerializer(serializers.Serializer):
     tenant_id = serializers.IntegerField(source="pk")
     tenant_name = serializers.CharField()
     apartment = serializers.CharField()
-    payment_status = serializers.CharField()
-    start_date = serializers.DateField()
-    end_date = serializers.DateField()
+    status = serializers.CharField()
+    date_joined = serializers.DateField()
 
 class TenancyCreateSerializer(serializers.Serializer):
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
@@ -22,23 +21,12 @@ class TenancyCreateSerializer(serializers.Serializer):
 class TenancyDetailSerializer(serializers.Serializer):
     tenant_id = serializers.IntegerField(source="pk")
     tenant_name = serializers.CharField()
-    phone_number = PhoneNumberSerializerField()
     apartment = serializers.CharField()
-    payment_status = serializers.CharField()
-    balance = serializers.DecimalField(max_digits=10, decimal_places=2)
-    admission_date = serializers.DateTimeField(source="created_at")
-    start_date = serializers.DateField()
-    end_date = serializers.DateField()
-    lease_rent = serializers.DecimalField(max_digits=10, decimal_places=2)
+    status = serializers.CharField()
+    date_joined = serializers.DateField()
+    phone_number = PhoneNumberSerializerField(source="user.account.phone_number")
+    paid_up_to = serializers.DateField()
 
-class TenancyUpdateSerializer(serializers.Serializer):
-    extension_months = serializers.IntegerField(min_value=1, max_value=4)
-    apartment = serializers.PrimaryKeyRelatedField(queryset=Apartment.objects.all())
 
-class TenancyExtendReservationSerializer(serializers.Serializer):
-    reservation_extension = serializers.DurationField()
-    tenancy = serializers.PrimaryKeyRelatedField(queryset=Tenancy.objects.all())
-
-class TenancyTerminateSerializer(serializers.Serializer):
-    tenancy = serializers.PrimaryKeyRelatedField(queryset=Tenancy.objects.all())
-    termination_date = serializers.DateField()
+class TenancyLeaseExtensionSerializer(serializers.Serializer):
+    duration_months = serializers.IntegerField()
