2ffb7b6fa1fc85c111276ddf835e92834c0cc6a0
munene25 <edmune25@gmail.com>
2026-05-29 15:48:38 +0300

    tenancy.urls: - trying to complete the views. - I would essentially split these up into admin and tenant.

--- tenancy/urls.py
+++ tenancy/urls.py
@@ -3,5 +3,6 @@ from tenancy import views
 
 urlpatterns = [
     path("", views.TenancyListCreateView.as_view(), name="list_create"),
-    path("<int:tenancy_id>", views.TenancyDetailUpdateDestroyView.as_view(), name="detail_update")
+    path("<int:tenancy_id>", views.TenancyDetailUpdateView.as_view(), name="detail_update"),
+    path("terminate/<int:tenancy_id>", views.TenancyTerminateView.as_view(), name="tenancy_terminate"),
 ]
