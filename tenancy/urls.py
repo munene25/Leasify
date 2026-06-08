9e0743dfe40144e59fb4ccc21890e6c26a0797c2
munene25 <edmune25@gmail.com>
2026-06-08 18:22:02 +0300

    tenancy.urls: - added routes for tenancy  in url config

--- tenancy/urls.py
+++ tenancy/urls.py
@@ -3,6 +3,8 @@ from tenancy import views
 
 urlpatterns = [
     path("", views.TenancyListCreateView.as_view(), name="list_create"),
-    path("<int:tenancy_id>", views.TenancyDetailUpdateView.as_view(), name="detail_update"),
-    path("terminate/<int:tenancy_id>", views.TenancyTerminateView.as_view(), name="tenancy_terminate"),
+    path("<int:tenancy_id>", views.TenancyDetailView.as_view(), name="detail_update"),
+    path("<int:tenancy_id>/terminate", views.TenancyTerminateView.as_view(), name="detail_update"),
+    path("<int:tenancy_id>/extend", views.TenancyLeaseExtensionView.as_view(), name="detail_update"),
+    path("termination_reasons", views.get_tenancy_termination_reasons, name="termination_reasons"),
 ]
