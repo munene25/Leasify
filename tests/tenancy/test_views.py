21599818f6dd4526454f714e5cc8fe2a1464d48b
munene25 <edmune25@gmail.com>
2026-06-10 18:03:35 +0300

    tests.tenancy.test_views: - super_user to superuser - list_create_view tests completed - started tests for tenancy detail

--- tests/tenancy/test_views.py
+++ tests/tenancy/test_views.py
@@ -1,6 +1,6 @@
 import pytest
+from unittest.mock import MagicMock
 from datetime import date
-from rest_framework.test import APIClient
 from tests.types import IsClient
 from tests.types import Factory
 from tests.helpers import *
@@ -8,6 +8,7 @@ from common.period import today
 from tenancy.models import Tenancy
 from users.models import User
 from tenancy.choices import TenancyStatus as TS, TerminationReason as TR
+from apartments.models import Apartment
 
 
 class TestTenancyListCreateView:
@@ -21,7 +22,7 @@ class TestTenancyListCreateView:
     @pytest.mark.parametrize(
         "_client,get,post",
         [
-            ("super_user_client", 200, 201),
+            ("superuser_client", 200, 201),
             ("manager_client", 200, 201),
             ("caretaker_client", 200, 201),
             ("tenant_client", 200, 201),
@@ -62,17 +63,17 @@ class TestTenancyListCreateView:
         tenancy_patch_validators,
     ):
         users = user_factory(5)
-        
-        terminated = tenancy_factory(5, users=users, overrides={"status": TS.TERMINATED})
-        defaulting = tenancy_factory(2, users=users[:2], overrides={"status": TS.DEFAULTING})
-        active = tenancy_factory(2, users=users[2:4], overrides={"status": TS.ACTIVE})
-        
+
+        terminated = tenancy_factory(5, users=users, status=TS.TERMINATED)
+        defaulting = tenancy_factory(2, users=users[:2], status=TS.DEFAULTING)
+        active = tenancy_factory(2, users=users[2:4], status=TS.ACTIVE)
+
         res1 = manager_client.get(self.path)
         parse_paginated_response(res1, len(terminated + defaulting + active))
 
         res2 = caretaker_client.get(self.path)
         parse_paginated_response(res2, len(terminated + active + defaulting))
-        
+
         # Authenticate on actual tenants
         # users[0] 1 terminated, 1 defaulting
         # users[2] 1 terminated, 1 active
@@ -80,7 +81,7 @@ class TestTenancyListCreateView:
         user_client.force_authenticate(users[0])
         data1 = parse_paginated_response(user_client.get(self.path), 2)
         assert {t["tenant_name"] for t in data1["results"]} == {users[0].full_name}
-        
+
         user_client.force_authenticate(users[2])
         data2 = parse_paginated_response(user_client.get(self.path), 2)
         assert {t["tenant_name"] for t in data2["results"]} == {users[2].full_name}
@@ -89,4 +90,101 @@ class TestTenancyListCreateView:
         data3 = parse_paginated_response(user_client.get(self.path), 1)
         assert {t["tenant_name"] for t in data3["results"]} == {users[4].full_name}
 
-        
+    @pytest.mark.parametrize(
+        "query_param,expected",
+        [
+            ("joined_after=2026-01-01", {"joined_after": date(2026, 1, 1)}),
+            ("joined_before=2026-12-31", {"joined_before": date(2026, 12, 31)}),
+            (
+                "joined_after=2026-01-01&joined_before=2026-12-31",
+                {"joined_after": date(2026, 1, 1), "joined_before": date(2026, 12, 31)},
+            ),
+            ("search=John", {"search": "John"}),
+            ("status=active", {"status": "active"}),
+        ],
+    )
+    def test_tenancy_list_query_filters(
+        self, monkeypatch, manager_user, manager_client: IsClient, query_param: str, expected: dict[str, Any]
+    ):
+        """
+        Patch list_for and test that filters are called with correct values.
+        """
+        mock = MagicMock()
+        monkeypatch.setattr("tenancy.selectors.tenancy_list_for", mock)
+
+        manager_client.get(self.path + f"?{query_param}")
+        mock.assert_called_once_with(user=manager_user, filters=expected)
+
+    def test_tenancy_create_response_format(self, user: User, user_client: IsClient, apartment: Apartment, today):
+        """User should be able to create a tenancy and response structure should be correct"""
+        response = user_client.post(self.path, data=self.data)
+        data = parse_message(response, status_code=201)
+        assert Tenancy.objects.count() == 1
+
+        assert data["user_id"] == user.pk
+        assert data["apartment_id"] == apartment.pk
+        assert data["tenancy_id"] == Tenancy.objects.first().pk  # type: ignore
+        assert data["tenant_name"] == user.full_name
+        assert data["apartment_name"] == apartment.apartment_name
+        assert data["status"] == TS.RESERVED
+        assert data["paid_up_to"] == None
+        assert data["date_joined"] == self.data["start_date"].isoformat()
+        assert data["created_at"] is not None
+        assert data["termination_reason"] is None
+        assert data["termination_date"] is None
+
+    def test_tenancy_create_calls(self, monkeypatch, user_client: IsClient, user: User, apartment: Apartment):
+        """Test that tenancy_create is called with correct values"""
+        mock = MagicMock(return_value=Tenancy())
+        monkeypatch.setattr("tenancy.services.tenancy_create", mock)
+
+        user_client.post(self.path, data=self.data)
+        mock.assert_called_once_with(
+            user=user,
+            apartment=apartment,
+            start_date=self.data["start_date"],
+            duration_months=self.data["duration_months"],
+        )
+
+
+class TestTenancyDetailView:
+    def path(self, pk: int):
+        return f"/tenancy/{str(pk)}"
+
+    @pytest.mark.parametrize(
+        "_client,get",
+        [
+            ("superuser_client",200,),
+            ("manager_client",200,),
+            ("caretaker_client",200,),
+            ("tenant_client",200,),
+            ("user_client",403,),
+            ("client",401,),
+        ],
+    )
+    def test_tenancy_detail_auth(self, _client: str, get: int, request, active_tenant: Tenancy):
+        """
+        Test auth and authorization for different types of clients
+        Active tenant is associated with the tenant_client.
+        """
+
+        client = request.getfixturevalue(_client)
+
+        parse_message(client.get(self.path(1)), get)
+    
+    def test_tenancy_detail_response(self, manager_client: IsClient, active_tenant: Tenancy):
+        """Active tenant has a paid period hence last paid"""
+
+        res1 = manager_client.get(self.path(1))
+        data1 = parse_message(res1)
+        assert data1["user_id"] == active_tenant.user_id
+        assert data1["apartment_id"] == active_tenant.apartment.pk
+        assert data1["tenancy_id"] == active_tenant.pk
+        assert data1["tenant_name"] == active_tenant.user.full_name
+        assert data1["apartment_name"] == active_tenant.apartment.apartment_name
+        assert data1["status"] == active_tenant.status
+        assert active_tenant.paid_up_to is not None
+        assert data1["paid_up_to"] == active_tenant.paid_up_to.isoformat()
+        assert data1["date_joined"] is not None
+        assert data1["termination_date"] is None
+        assert data1["termination_reason"] is None
