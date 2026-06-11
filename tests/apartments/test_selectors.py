a0149e53f695e06bb9b8d05c7997aec3839e12be
munene25 <edmune25@gmail.com>
2026-06-11 21:26:24 +0300

    tests.apartments.test_selectors: - include tests for filtering via query params

--- tests/apartments/test_selectors.py
+++ tests/apartments/test_selectors.py
@@ -7,7 +7,6 @@ from django.utils import timezone
 
 today = lambda: timezone.now().date()
 
-
 class TestApartmentList:
 
     def test_apartment_for_regular_user(self, user_factory, apartment_factory, tenancy_patch_validators):
@@ -30,3 +29,31 @@ class TestApartmentList:
         assert list_2_for_user1.count() == 4
         list_2_for_user2 = apartment_list_for(user=users[1], filters={})
         assert list_2_for_user2.count() == 4
+        
+    
+    def test_apartment_filters_based_on_filters(self, manager_user, apartment_factory):
+        """test that apartments can be filtered based on "rentable" or "search" params"""
+
+        apartment_factory(block="OLD", unit_number=1, rentable=True, rent=12000)
+        apartment_factory(block="NEW", unit_number=2, rentable=True, rent=15000)
+        apartment_factory(block="OLD", unit_number=3, rentable=False, rent=20000)
+        
+        # test that "rentable" filter works
+        assert apartment_list_for(user=manager_user, filters={"rentable": True}).count() == 2
+        assert apartment_list_for(user=manager_user, filters={"rentable": False}).count() == 1
+
+        # test that "search" filter works
+        assert apartment_list_for(user=manager_user, filters={"search": "OLD"}).count() == 2
+        assert apartment_list_for(user=manager_user, filters={"search": "NEW"}).count() == 1
+
+        # test "search" and "rentable" works together
+        apt = apartment_list_for(user=manager_user, filters={"search": "OLD", "rentable": False})
+        assert apt.latest("pk").pk == 3
+
+        # test "unit number" search works
+        apt = apartment_list_for(user=manager_user, filters={"search": "3"})
+        assert apt.latest("pk").unit_number == 3
+
+        # test "rent" works
+        assert apartment_list_for(user=manager_user, filters={"rent_min": 20000}).count() == 1
+        
