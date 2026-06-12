e0b77da30d7769f84ddcf77224908f8e71e25749
munene25 <edmune25@gmail.com>
2026-06-12 19:35:08 +0300

    billing.serializer: - added serializers for billing views

---
+++ billing/serializer.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from billing.choices import BillingStatus
+from payments.models import Payment
+
+
+class BillingListSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    billing_id = serializers.IntegerField()
+    tenancy_id = serializers.IntegerField()
+    tenant_name = serializers.CharField(source="tenancy.user.full_name")
+    status = serializers.ChoiceField(choices=BillingStatus.choices)
+    duration_months = serializers.IntegerField()
+    is_current = serializers.BooleanField()
+
+
+class BillingDetailSerializer(serializers.Serializer):
+    start_date = serializers.DateField()
+    end_date = serializers.DateField()
+    total_due = serializers.DecimalField(max_digits=10, decimal_places=2)
+    status = serializers.ChoiceField(choices=BillingStatus.choices)
+    is_current = serializers.BooleanField()
+    duration_months = serializers.IntegerField()
+
+class BillingCompleteSerializer(serializers.Serializer):
+    payment = serializers.PrimaryKeyRelatedField(queryset=Payment.objects.all())
