feb1da8a0c660fcc0c3e7b30285a24ce547c2278
munene25 <edmune25@gmail.com>
2026-06-12 19:57:43 +0300

    payments.models: - paid_by instead of payee.

--- payments/models.py
+++ payments/models.py
@@ -8,13 +8,17 @@ class Payment(BaseModel):
     """
     Payment will have 2 states, once initiated and then confirmed
     On confirmation, status changes to either FAILED or SUCCESS.
-    It's okay to duplicate the payee even 
+    ! There is a duplicate of the payee, might switch to User foreign key though to capture both inititor and paid_by
+    ? paid_by though captures the name of the mpesa user, but actually the mpesa callback does not contain the name.
     """
     class Meta:
+        ordering = ["-created_at"]
         indexes = [
             models.Index(fields=["status"])
         ]
-
+        permissions = [
+            ("add_payment_manually", "Can manually create a payment with status outside of PENDING"),
+        ]
 
     billing = models.ForeignKey(BillingPeriod, null=False, blank=False, on_delete=models.PROTECT, related_name="payments")
     receipt_no = models.CharField(null=True, blank=True)
@@ -23,7 +27,7 @@ class Payment(BaseModel):
     checkout_id = models.CharField(db_index=True, unique=True, null=False, blank=False)
     amount = models.DecimalField(decimal_places=2, max_digits=10, editable=False, blank=False, null=False)
     status = models.CharField(null=False, blank=False, choices=PaymentStatus, default=PaymentStatus.PENDING)
-    payee = models.CharField(max_length=30, null=True, blank=True)
+    paid_by = models.CharField(max_length=30, null=True, blank=True, editable=False)
 
     billing_id: int
 
