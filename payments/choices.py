12a95d3e65c14f3c341627e8050a4ae0952e644e
munene25 <edmune25@gmail.com>
2026-06-12 19:57:21 +0300

    payments.choices: - removed system initiator - added docstring for better suggestions

--- payments/choices.py
+++ payments/choices.py
@@ -1,11 +1,12 @@
 from django.db.models import TextChoices
 
 class PaymentStatus(TextChoices):
+    """PENDING, SUCCESS or FAILED STATUS"""
     PENDING = "pending", "Payment has been initiated but not confirmed"
     SUCCESS = "success", "Payment has been confirmed and funds have transferred"
     FAILED = "failed", "Payment could not be completed"
 
 class PaymentInitiator(TextChoices):
-    SYSTEM = "SGT", "System initiated"
+    """TENANT OR ADMIN INITIATED PAYMENT"""
     TENANT = "TPY", "System initiated"
     ADMIN = "APY", "System initiated"
