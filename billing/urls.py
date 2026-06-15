c2432cdef7857c2a266450e7d03c6f37c1bcebf1
munene25 <edmune25@gmail.com>
2026-06-15 11:59:53 +0300

    billing.urls: - Bug Fix: urlpatterns was incorrectly named

--- billing/urls.py
+++ billing/urls.py
@@ -1,7 +1,7 @@
-from billing import views
 from django.urls import path
+from billing import views
 
-url_patterns = [
+urlpatterns = [
     path("", views.BillingListView.as_view(), name="billing_list"),
     path("<int:billing_id>", views.BillingDetailView.as_view(), name="billing_detail"),
     path("<int:billing_id>/cancel", views.BillingCancelView.as_view(), name="billing_detail"),
