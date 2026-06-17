c8ff284c86c7b1695cc403fa9c289d581999e532
munene25 <edmune25@gmail.com>
2026-06-17 18:44:26 +0300

    tests.billing.test_selectors: - slight formatting and cancelled renaming

--- tests/billing/test_selectors.py
+++ tests/billing/test_selectors.py
@@ -18,12 +18,19 @@ def test_number_of_queries(tenancy, django_assert_num_queries):
         bp = sl.BASE_QS.get(pk=1)
         assert bp.tenancy == tenancy
         assert bp.tenancy.user is not None
-            
+
 
 class TestBillingListFor:
-    def test_role_based_filtering(self, superuser: User, manager_user: User, caretaker_user: User, tenancy_factory: Factory[Tenancy], billing_factory: Factory[BP]):
+    def test_role_based_filtering(
+        self,
+        superuser: User,
+        manager_user: User,
+        caretaker_user: User,
+        tenancy_factory: Factory[Tenancy],
+        billing_factory: Factory[BP],
+    ):
         """Test that the role based filtering works"""
-        
+
         tset = tenancy_factory(2)
         bills_1 = billing_factory(quantity=2, tenancy=tset[0])
         bills_2 = billing_factory(quantity=3, tenancy=tset[1])
@@ -36,37 +43,42 @@ class TestBillingListFor:
         fetched_2 = sl.billing_list_for(user=tset[1].user)
         assert {b.pk for b in bills_2} == set(fetched_2.values_list("pk", flat=True))
 
-    
     def test_filtering_based_on_query_params(self, manager_user, user_factory, billing_factory, tenancy_factory, today):
         """
         Can filter correctly based on filters dict
         Should be able to filter based on status, is_current, period, search: "user_first_name", "user_last_name"
         """
+
         def query(filters: dict[str, str]):
-            return sl.billing_list_for(user=manager_user, filters= filters)
-        
+            return sl.billing_list_for(user=manager_user, filters=filters)
+
         user1 = user_factory(first_name="John", last_name="Doe")
         user2 = user_factory(first_name="Alex", last_name="Smith")
-        t = tenancy_factory(users=(user1+user2))
-        
-        billing_factory(tenancy=t[0], statuses=[BS.PAID, BS.CANCELED, BS.UNPAID], starting=date(2024, 1, 1))
-        billing_factory(tenancy=t[1], statuses=[BS.CANCELED, BS.PAID], starting=date(2024, 2, 1))
-        
+        t = tenancy_factory(users=(user1 + user2))
+
+        billing_factory(tenancy=t[0], statuses=[BS.PAID, BS.CANCELLED, BS.UNPAID], starting=date(2024, 1, 1))
+        billing_factory(tenancy=t[1], statuses=[BS.CANCELLED, BS.PAID], starting=date(2024, 2, 1))
+
         assert len(query({"status": "paid"})) == 2
         assert len(query({"status": "unpaid"})) == 1
         assert len(query({"period_after": "2024-1-1"})) == 5
         assert len(query({"period_after": "2024-2-1", "period_before": "2024-2-28"})) == 2
         assert len(query({"search": "Doe"})) == 3
         assert len(query({"search": "Ale"})) == 2
-        
+
         with freeze_time("2024-2-1"):
             assert len(query({"is_current": "true"})) == 2
 
 
-
-def test_billing_get_for_role_based_filtering(superuser: User, manager_user: User, caretaker_user: User, tenancy_factory: Factory[Tenancy], billing_factory: Factory[BP]):
+def test_billing_get_for_role_based_filtering(
+    superuser: User,
+    manager_user: User,
+    caretaker_user: User,
+    tenancy_factory: Factory[Tenancy],
+    billing_factory: Factory[BP],
+):
     """Test that the role based filtering works"""
-    
+
     tset = tenancy_factory(2)
     bills_1 = billing_factory(quantity=2, tenancy=tset[0])
     bills_2 = billing_factory(quantity=3, tenancy=tset[1])
@@ -83,7 +95,7 @@ def test_billing_last_paid_for(tenancy_factory: Factory[Tenancy], billing_factor
     """Raises if not found otherwise returns the last billing period"""
     t = tenancy_factory(2)
     billing_factory(tenancy=t[0], statuses=[BS.PAID, BS.PAID, BS.PAID], starting=date(2020, 1, 1))
-    billing_factory(tenancy=t[1], statuses=[BS.CANCELED, BS.CANCELED, BS.UNPAID])
+    billing_factory(tenancy=t[1], statuses=[BS.CANCELLED, BS.CANCELLED, BS.UNPAID])
 
     last_paid = sl.billing_last_paid(t[0])
     # 1 Month each so the next billing is on the 1st of March
