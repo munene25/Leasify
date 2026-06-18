06e22e3866a4119c06e5877d1f052f0dddff41ea
munene25 <edmune25@gmail.com>
2026-06-18 15:16:54 +0300

    payments.urs: - added routes for payment related views

--- payments/urls.py
+++ payments/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import include, path
+from payments import views
 
 urlpatterns = [
-    # path("", views.PaymentListView.as_view(), name="payment_list"),
-    
+    # Payment List - List and retrieve all payments accessible by the authenticated user
+    path("list/", views.PaymentListView.as_view(), name="payment_list"),
+    # Initiate M-Pesa payment via STK push
+    path("initiate/", views.PaymentInitiateView.as_view(), name="payment_initiate"),
+    # Callback endpoint for M-Pesa to update payment status
+    path("mpesa/callback/", views.PaymentMpesaCallbackView.as_view(), name="payment_callback"),
+    # Manually create a payment via alternate mode (CASH/BANK)
+    path("alt/create/", views.PaymentAltCreateView.as_view(), name="payment_alt_create"),
 ]
