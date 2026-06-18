33233c69ca2df4f74733e84bb453f1c341789ef0
munene25 <edmune25@gmail.com>
2026-06-18 19:50:07 +0300

    scripts.testing: - just testing emails

--- scripts/testing.py
+++ scripts/testing.py
@@ -12,17 +12,17 @@ from tenancy.choices import TenancyStatus as TS, TerminationReason as TR
 from billing.choices import BillingStatus as BS
 from payments import models as p_models, selectors as p_selectors, services as p_services
 from users import services as u_services, models as u_models, selectors as u_selectors
-from payments.choices import PaymentInitiator
+from payments.tasks import send_payment_notification
+from tenancy.tasks import notify_reserved_on_expiry
 
 def run():
-    
-    tenant = t_selectors.tenancy_in([TS.RESERVED]).latest("created_at")
-    # t_services.tenancy_renew_lease(tenant, 2)
-    billing = tenant.billings.latest("start_date")
-    if billing:
-        p_services.payment_initiate(
-            billing, 
-            "254791573104", 
-            initiator=PaymentInitiator.ADMIN, 
-            stk_push=True
-        ) 
+    # user = u_selectors.user_get(18)
+    # apartment = a_selectors.apartment_get_for(user=user, apartment_id=15)
+    # tenant = t_services.tenancy_create(user=user, apartment=apartment, start_date=date.today(), duration_months=2)
+    # tenant.reservation_expiry = tenant.reservation_expiry - timedelta(1)
+    # tenant.save()
+    tenant = t_models.Tenancy.objects.get(pk=17)
+    tenant.reservation_expiry = date.today() + timedelta(1)
+    tenant.save()
+    # send_payment_notification(1)
+    notify_reserved_on_expiry()
