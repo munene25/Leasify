fab67cc069efcfe5c7c92a5e72c63c47355ccfe3
munene25 <edmune25@gmail.com>
2026-06-18 21:25:15 +0300

    payments.views: - renamed parse_mpesa_response, no need to serialize

--- payments/views.py
+++ payments/views.py
@@ -6,10 +6,9 @@ from common.pagination import get_paginated_response
 from common.permissions import check_perms
 from payments import services as sr, selectors as sl, serializer as sc
 from common.views import BaseAPIView
-from payments.mpesa import parse_response
+from payments.mpesa import parse_mpesa_response
 from billing.selectors import billing_get_for
 from django.http import HttpResponse
-
 
 class PaymentListView(BaseAPIView):
     """List and retrieve all payments accessible by the authenticated user."""
@@ -67,10 +66,9 @@ class PaymentMpesaCallbackView(BaseAPIView):
     """Update payment status based on callback response."""
 
     def post(self, request) -> Response:
-        cb = parse_response(request.data)
-        payment = sr.payment_mpesa_confirm(cb=cb)
-        outgoing = sc.PaymentDetailSerializer(instance=payment)
-        return Response(data=outgoing.data, status=status.HTTP_200_OK)
+        cb = parse_mpesa_response(request.data)
+        sr.payment_mpesa_process(cb=cb)
+        return Response(status=status.HTTP_200_OK)
 
 
 class PaymentAltCreateView(BaseAPIView):
