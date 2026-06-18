2d9c5723d1f46e7b3b225c11e2c4f78a03736a11
munene25 <edmune25@gmail.com>
2026-06-18 15:26:56 +0300

    tenancy.admin: - renamed apartment name to name, minor reformating

--- tenancy/admin.py
+++ tenancy/admin.py
@@ -10,14 +10,17 @@ from tenancy.selectors import BASE_QS
 logger = get_logger("tenancy.admin")
 
 
-
 @admin.register(Tenancy)
 class TenancyAdmin(admin.ModelAdmin):
     list_display = ("pk", "apartment_name", "tenant_name", "status", "billings")
     list_filter = ("status", "termination_reason")
-    
-    search_fields = ("tenant__user__first_name", "tenant__user__last_name", "apartment__unit_number", "apartment__block__name")
-    
+
+    search_fields = (
+        "tenant__user__first_name",
+        "tenant__user__last_name",
+        "apartment__unit_number",
+        "apartment__block__name",
+    )
 
     fields = (
         "apartment",
@@ -26,7 +29,7 @@ class TenancyAdmin(admin.ModelAdmin):
         "created_at",
         "reservation_expiry",
         "termination_reason",
-        "termination_date"
+        "termination_date",
     )
     readonly_fields = ("created_at", "reservation_expiry")
     actions = ["terminate",]
@@ -34,10 +37,10 @@ class TenancyAdmin(admin.ModelAdmin):
     def tenant_name(self, obj):
         url = reverse("admin:users_user_change", args=[obj.pk])
         return format_html('<a href="{}">{}</a>', url, obj.user.full_name)
-    
+
     def apartment_name(self, obj):
         url = reverse("admin:apartments_apartment_change", args=[obj.pk])
-        return format_html('<a href="{}">{}</a>', url, obj.apartment.apartment_name)
+        return format_html('<a href="{}">{}</a>', url, obj.apartment.name)
 
     def billings(self, obj):
         url = reverse("admin:billing_billingperiod_changelist")
@@ -47,5 +50,3 @@ class TenancyAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request: HttpRequest) -> QuerySet:
         return BASE_QS
-
-
