f7ec4c94e9b18e8847ba48953cb941bf6b5fcf06
munene25 <edmune25@gmail.com>
2026-06-18 16:08:31 +0300

    payments.serializer: - some modifications to the choice fields and status fields - we might need to move permernently to ids now - nested-serializers might be the way to go eventually

--- payments/serializer.py
+++ payments/serializer.py
@@ -4,26 +4,25 @@ from billing.models import BillingPeriod as BP
 
 class PaymentListSerializer(serializers.Serializer):
     """Serializer for listing payments"""
-    id = serializers.IntegerField(source="pk")
+    payment_id = serializers.IntegerField(source="pk")
     ref_no = serializers.CharField()
     billing_id = serializers.IntegerField()
+    billing = serializers.CharField(source="billing.name")
     amount = serializers.DecimalField(max_digits=10, decimal_places=2)
-    status = serializers.ChoiceField(choices=PaymentStatus.choices)
-    phone_number = serializers.CharField()
-    initiated_by = serializers.CharField()
+    status = serializers.CharField()
 
 
 class PaymentDetailSerializer(serializers.Serializer):
     """Serializer for payment detail"""
-    id = serializers.IntegerField(source="pk")
+    payment_id = serializers.IntegerField(source="pk")
     ref_no = serializers.CharField()
     billing_id = serializers.IntegerField()
     tenant_name = serializers.CharField(source="billing.tenancy.user.full_name")
     amount = serializers.DecimalField(max_digits=10, decimal_places=2)
-    status = serializers.ChoiceField(choices=PaymentStatus.choices)
+    status = serializers.CharField()
     phone_number = serializers.CharField()
     initiated_by = serializers.CharField()
-    receipt_no = serializers.CharField(allow_null=True, source="receipt_no", default=None)
+    receipt_no = serializers.CharField()
 
 class PaymentInitiateMpesaSerializer(serializers.Serializer):
     """Payment initiation serializer for M-Pesa STK push"""
@@ -35,5 +34,5 @@ class PaymentAltCreateSerializer(serializers.Serializer):
     """Serializer for manual payment creation (alternate mode: CASH/BANK)"""
     billing_id = serializers.PrimaryKeyRelatedField(queryset=BP.objects.all())
     mode = serializers.ChoiceField(choices=PaymentMode.choices, required=False)
-    status = serializers.CharField(choices=PaymentStatus.choices)
+    status = serializers.ChoiceField(choices=PaymentStatus.choices)
     recorded_by = serializers.CharField()
