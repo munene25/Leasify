54a0a799ea25cee99a43a44949f7ed9e83765dbc
munene25 <edmune25@gmail.com>
2026-06-18 16:35:55 +0300

    payments.urls: - renamed PyamentInitiate to include the initiator name in the class name

--- payments/urls.py
+++ payments/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # Payment List - List and retrieve all payments accessible by the authenticated user
     path("list/", views.PaymentListView.as_view(), name="payment_list"),
     # Initiate M-Pesa payment via STK push
-    path("initiate/", views.PaymentInitiateView.as_view(), name="payment_initiate"),
+    path("initiate/", views.PaymentInitiateMpesaView.as_view(), name="payment_initiate"),
     # Callback endpoint for M-Pesa to update payment status
     path("mpesa/callback/", views.PaymentMpesaCallbackView.as_view(), name="payment_callback"),
     # Manually create a payment via alternate mode (CASH/BANK)
