fadd2de6ee2b18434ad1a42ae8ea7e43c717c75b
munene25 <edmune25@gmail.com>
2026-06-18 16:46:52 +0300

    payments.urls: - added billing ids to the creation views

--- payments/urls.py
+++ payments/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     # Payment List - List and retrieve all payments accessible by the authenticated user
     path("list/", views.PaymentListView.as_view(), name="payment_list"),
     # Initiate M-Pesa payment via STK push
-    path("initiate/", views.PaymentInitiateMpesaView.as_view(), name="payment_initiate"),
+    path("initiate/<int:billing_id>", views.PaymentInitiateMpesaView.as_view(), name="payment_initiate"),
     # Callback endpoint for M-Pesa to update payment status
     path("mpesa/callback/", views.PaymentMpesaCallbackView.as_view(), name="payment_callback"),
     # Manually create a payment via alternate mode (CASH/BANK)
-    path("alt/create/", views.PaymentAltCreateView.as_view(), name="payment_alt_create"),
+    path("alt/create/<int:billing_id>", views.PaymentAltCreateView.as_view(), name="payment_alt_create"),
 ]
