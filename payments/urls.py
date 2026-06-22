5a8a8d9c75dbf5984a620bc795209a0875d1f7fa
munene25 <edmune25@gmail.com>
2026-06-22 18:30:21 +0300

    payments.urls: - refactored the urls to be more restful

--- payments/urls.py
+++ payments/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 from payments import views
 
 urlpatterns = [
-    # Payment List - List and retrieve all payments accessible by the authenticated user
+    # List and retrieve all payments accessible by the authenticated user
     path("", views.PaymentListView.as_view(), name="payment_list"),
-    # Initiate M-Pesa payment via STK push
-    path("mpesa/initiate/<int:billing_id>", views.PaymentInitiateMpesaView.as_view(), name="payment_initiate"),
+    # Retrieve specific payment information
+    path("<int:payment_id>/", views.PaymentDetailView.as_view(), name="payment_detail"),
+    # Initiate M-Pesa payment via STK push (billing_id in POST data, supports idempotency key)
+    path("mpesa/initiate/", views.PaymentInitiateMpesaView.as_view(), name="payment_initiate_mpesa"),
     # Callback endpoint for M-Pesa to update payment status
     path("mpesa/callback/", views.PaymentMpesaCallbackView.as_view(), name="payment_callback"),
-    # Manually create a payment via alternate mode (CASH/BANK)
-    path("alt/create/<int:billing_id>", views.PaymentAltCreateView.as_view(), name="payment_alt_create"),
-    # Payment detail endpoint - Retrieve specific payment information
-    path("<int:payment_id>", views.PaymentDetailView.as_view(), name="payment_detail"),
+    # Manually create a payment via alternate mode (CASH/BANK) (billing_id in POST data)
+    path("alt/", views.PaymentAltCreateView.as_view(), name="payment_alt_create"),
     # Query and update M-PESA payment status
-    path("<int:payment_id>/query", views.PaymentStatusQueryView.as_view(), name="query_payment_status"),
+    path("<int:payment_id>/query/", views.PaymentStatusQueryView.as_view(), name="payment_query_status"),
 ]
