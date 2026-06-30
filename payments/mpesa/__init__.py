c85c59caa8b8fb5d2b07d02d40f4a1afbb4ca253
munene25 <edmune25@gmail.com>
2026-06-30 17:18:32 +0300

    payments.mpesa.__init__: - changed stkr esponse to stk initial response

--- payments/mpesa/__init__.py
+++ payments/mpesa/__init__.py
@@ -1,4 +1,4 @@
 from .parser import parse_callback_response
-from .types import STKResult, STKPushResponse
+from .types import STKResult, STKInitialResponse
 from .utils import make_timestamp, make_password
 from .handlers import initiate_stk_push, query_payment_status
