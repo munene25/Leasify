e7948aba992dadad7420869b8b37bf0495788c6a
munene25 <edmune25@gmail.com>
2026-07-03 15:16:36 +0300

    payments.tasks: - added payment processing async

--- payments/tasks.py
+++ payments/tasks.py
@@ -1,9 +1,12 @@
 from payments.models import PaymentStatus, Payment
-from config.emails import send_template_email
+from payments import services as sr
+from common.emails import send_template_email
+from common.tasks import email_task, regular_task
+from payments.mpesa import STKResult
 
-
-def send_payment_notification(payment_id: int, additional_recepients: list[str] = []):
-    """Send payment receipt email to the user with PDF attachment."""
+@email_task
+def send_payment_notification(payment_id: int, additional_recepients: list[str] = []) -> dict[str, list]:
+    """Send payment receipt email to the user and additional recepients"""
 
     payment = Payment.objects.select_related("billing__tenancy__apartment", "billing__tenancy__user").get(pk=payment_id)
     user = payment.billing.tenancy.user
@@ -15,11 +18,17 @@ def send_payment_notification(payment_id: int, additional_recepients: list[str] 
         "status": "SUCCESS" if payment.status == PaymentStatus.SUCCESS else "FAILED",
         "receipt_date": payment.created_at.strftime("%d %b %Y, %I:%M %p"),
     }
-    # ? Also send to the maager/caretaker etc
-    # ? In order to do so the email should be as generic as possible.
+    # In order to send emails to both the payer and additional_recepients the email should be as generic as possible.
+    recepients = [user.email, *additional_recepients]
     send_template_email(
         subject="Payment received",
         context=context,
-        to=[user.email, *additional_recepients], 
+        to=recepients, 
         template_name="payments/payment_notification",
     )
+    return {"Payment notification sent": recepients}
+
+@regular_task
+def payment_mpesa_process_async(stk_result: STKResult):
+    """This is an async processor for mpesa callbacks"""
+    sr.payment_mpesa_process(stk_result)
