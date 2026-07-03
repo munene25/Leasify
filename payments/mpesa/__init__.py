3bbbc0fe7a38717a33e695b0139bd53f744e1b8f
munene25 <edmune25@gmail.com>
2026-07-03 15:13:24 +0300

    payments.mpesa: - added auth to init

--- payments/mpesa/__init__.py
+++ payments/mpesa/__init__.py
@@ -2,3 +2,4 @@ from .parser import parse_callback_response
 from .types import STKResult, STKInitialResponse
 from .utils import make_timestamp, make_password
 from .handlers import initiate_stk_push, query_payment_status
+from .auth import get_access_token
