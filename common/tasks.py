db70c388ea69dba75c66873818c8c5fefa3f17b3
munene25 <edmune25@gmail.com>
2026-07-03 13:54:47 +0300

    common.tasks: - added a common task decorator for email sending

---
+++ common/tasks.py
@@ -0,0 +1,7 @@
+from typing import Callable
+import smtplib
+from celery import Task,  shared_task
+
+
+# A common task decorator for async email sending tasks
+email_task: Callable[..., Task] = shared_task(autoretry_for=(smtplib.SMTPDataError, ConnectionError), retry_kwargs={'max_retries': 3}, retry_backoff=True, retry_jitter=True)
