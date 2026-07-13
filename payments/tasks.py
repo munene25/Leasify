b2a28be5a1a42b368658b6f8a573a1021211d434
munene25 <edmune25@gmail.com>
2026-07-13 19:46:30 +0300

    payments.tasks: - no need for context, payment object contains all context

--- payments/tasks.py
+++ payments/tasks.py
@@ -10,19 +10,11 @@ def send_payment_notification(payment_id: int, additional_recepients: list[str] 
 
     payment = Payment.objects.select_related("billing__tenancy__apartment", "billing__tenancy__user").get(pk=payment_id)
     user = payment.billing.tenancy.user
-
-    context = {
-        "received_from": user.full_name,
-        "receipt_no": payment.receipt_no or "N/A",
-        "payment": payment,
-        "status": "SUCCESS" if payment.status == PaymentStatus.SUCCESS else "FAILED",
-        "receipt_date": payment.created_at.strftime("%d %b %Y, %I:%M %p"),
-    }
     # In order to send emails to both the payer and additional_recepients the email should be as generic as possible.
     recepients = [user.email, *additional_recepients]
     send_template_email(
         subject="Payment received",
-        context=context,
+        context={"payment": payment},
         to=recepients, 
         template_name="payments/payment_notification",
     )
