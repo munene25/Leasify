054664300faba3cbaf727f1a66aaa85a25efae5f
munene25 <edmune25@gmail.com>
2026-07-15 13:30:06 +0300

    payments.tasks: - refactored how mpesa process async is handled. It now performs notification and updating the billing period

--- payments/tasks.py
+++ payments/tasks.py
@@ -1,26 +1,52 @@
-from payments.models import PaymentStatus, Payment
-from payments import services as sr
+from django.db import transaction
 from common.emails import send_template_email
-from common.tasks import email_task, regular_task
+from common.tasks import email_task, critical_task
+from billing.services import billing_period_complete
 from payments.mpesa import STKResult
+from payments.models import Payment
+from payments import selectors as sl, services as sr
+from payments.choices import PaymentStatus as PS
+
 
 @email_task
-def send_payment_notification(payment_id: int, additional_recepients: list[str] = []) -> dict[str, list]:
+def send_payment_notification(payment_id: int, additional_recepients: list[str] = []) -> list[str]:
     """Send payment receipt email to the user and additional recepients"""
 
     payment = Payment.objects.select_related("billing__tenancy__apartment", "billing__tenancy__user").get(pk=payment_id)
     user = payment.billing.tenancy.user
-    # In order to send emails to both the payer and additional_recepients the email should be as generic as possible.
+
+    # Fetch additional_recepients 
     recepients = [user.email, *additional_recepients]
+    
+    # Multiple recepients require email to be generic.
     send_template_email(
-        subject="Payment received",
+        subject="Payment has been received",
         context={"payment": payment},
-        to=recepients, 
+        to=recepients,
         template_name="payments/payment_notification",
     )
-    return {"Payment notification sent": recepients}
+    return recepients
 
-@regular_task
-def payment_mpesa_process_async(stk_result: STKResult):
+
+@critical_task
+@transaction.atomic
+def payment_mpesa_process_async(stk_result: STKResult) -> dict[str, bool | str]:
     """This is an async processor for mpesa callbacks"""
-    sr.payment_mpesa_process(stk_result)
+
+    payment = sr.payment_mpesa_process(stk_result)
+
+    if payment.status == PS.SUCCESS:
+        billing = billing_period_complete(payment.billing)
+        extra_recepients = sl.payment_get_extra_recepients()
+        send_payment_notification.delay(payment.pk, extra_recepients)
+        notification = True
+    else:
+        billing = payment.billing
+        notification = False
+
+    return {
+        "payment": payment.status,
+        "billing": billing.status,
+        "tenancy": billing.tenancy.status,
+        "notify": notification,
+    }
