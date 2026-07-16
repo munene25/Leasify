1c549d04846af0aea84c0ba88d6a2351e59e91c2
munene25 <edmune25@gmail.com>
2026-07-16 09:56:08 +0300

    tests.billing.conftest: - moved patch billing to global context

--- tests/billing/conftest.py
+++ tests/billing/conftest.py
@@ -7,15 +7,6 @@ from billing.choices import BillingStatus as BS
 from django.db.models import QuerySet
 
 
-
-
-@pytest.fixture()
-def patch_billing_get_for(monkeypatch: MonkeyPatch, billing_factory: Factory[BillingPeriod]):
-    billing = billing_factory(1, statuses=[BS.UNPAID])[0]
-    mock = MagicMock(return_value=billing)
-    monkeypatch.setattr("billing.selectors.billing_get_for", mock)
-    yield mock
-
 @pytest.fixture()
 def patch_billing_list_for(monkeypatch: MonkeyPatch):
     mock = MagicMock(return_value=QuerySet(BillingPeriod))
