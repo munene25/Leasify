b76aaef96e8682bfa1276f6cc88d6882cbc088c0
munene25 <edmune25@gmail.com>
2026-07-23 13:53:42 +0300

    refactor(models): reverted block field default property. - reformatted and added docstrings as well

--- apartments/models.py
+++ apartments/models.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 from typing import TYPE_CHECKING
 from django.db import models
 from common.models import BaseModel
@@ -20,7 +19,6 @@ class Apartment(BaseModel):
         ordering = ["-created_at"]
         unique_together = "block", "unit_number"
 
-    
     block = models.CharField(max_length=10, choices=Block.choices, blank=False, null=False)
     unit_number = models.PositiveSmallIntegerField(blank=False, null=False)
     floor = models.PositiveSmallIntegerField(blank=False, null=False, help_text="Floor number, 0 for ground floor")
@@ -29,37 +27,31 @@ class Apartment(BaseModel):
 
     wing = models.CharField(max_length=15, blank=True, null=True, choices=Wing.choices)
 
-
-    tenancy_set: models.QuerySet[Tenancy]
-    _active_tenant: list[Tenancy]
+    tenancy_set: models.QuerySet["Tenancy"]
+    _active_tenant: list["Tenancy"]
 
     def __str__(self) -> str:
+        """Simplified representation of the apartment"""
         return f"Block-{self.block} Unit-{self.unit_number:02}"
 
     @property
     def name(self) -> str:
-        """
-        Full represantation of the aparment block, unit_number, floor, wing.
-        """
+        """Full represantation of the aparment block, unit_number, floor, wing."""
+
         parts = [f"Unit {self.block}-{self.floor}{self.unit_number:02}"]
         if self.wing:
             parts.append(f"{self.wing.capitalize()} wing")
         return " | ".join(parts)
 
     @property
-    def current_tenant(self) -> Tenancy | None:
-        """
-        Return the current tenant if they exist.
-        """
-        current = getattr(self, "_active_tenant", None)
+    def current_tenant(self) -> "Tenancy | None":
+        """Return the current tenant if they exist."""
 
-        if not current:
-            return self.tenancy_set.select_related("user").filter(status__in=ACTIVE_RESERVED_OR_DEFAULTING).first()
-        return next(iter(current), None)
+        if current:= getattr(self, "_active_tenant", None):
+            return current[0]
+        return self.tenancy_set.select_related("user").filter(status__in=ACTIVE_RESERVED_OR_DEFAULTING).first()
 
     @property
     def is_occupied(self) -> bool:
-        """
-        Whether or not the apartment is currently occupied.
-        """
+        """Whether or not the apartment is currently occupied."""
         return bool(self.current_tenant)
--- tests/apartments/conftest.py
+++ tests/apartments/conftest.py
@@ -1,2 +1,15 @@
 import pytest
 from tests.types import Factory
+from apartments.choices import Wing, Block
+
+
+@pytest.fixture
+def apartment_data() -> dict[str, str| int]:
+    return {
+        "block": Block.A,
+        "unit_number": 1,
+        "floor": 0,
+        "rent": 10_000,
+        "rentable": True,
+        "wing": Wing.WEST,
+    }
