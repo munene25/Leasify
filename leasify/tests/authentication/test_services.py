a74b5533c42a2ea2f1b236d316e2ac9e48eb1ec1
munene25 <edmune25@gmail.com>
2026-08-01 22:03:21 +0300

    refactor(tests): reorganize email verification and password change tests for clarity

--- leasify/tests/authentication/test_services.py
+++ leasify/tests/authentication/test_services.py
@@ -1,21 +1,11 @@
 import pytest
+from unittest.mock import MagicMock
 
-from django.core.mail import EmailMessage
+
 from rest_framework.exceptions import ValidationError, AuthenticationFailed
 
 from leasify.authentication import services as s
 from leasify.users.models import User
-
-
-class TestUserEmailVerifyConfirmation:
-    def test_email_set_as_verified(self, user: User):
-        """
-        Verified status should reflect
-        """
-        mod_user = s.user_email_verify(user)
-        mod_user.refresh_from_db() # type: ignore
-        assert mod_user == user
-        assert mod_user.verified == True
 
 
 class TestLoginService:
@@ -53,17 +43,28 @@ class TestLoginService:
         assert authd_user == user
 
 
+
+class TestUserEmailVerifyConfirmation:
+    def test_email_set_as_verified(self, user: User,):
+        """
+        Verified status should reflect
+        """
+        mod_user = s.user_email_verify(user)
+        mod_user.refresh_from_db() # type: ignore
+        assert mod_user == user
+        assert mod_user.verified == True
+
+
 class TestUserChangePassword:
-    def test_password_change_successful(self, user: User) -> None:
+    def test_case_for_changing_password(self, user: User, password: str, patch_notify_password_change: MagicMock) -> None:
         """
         A users password be hashed
         The new password should work pass check_password
         The old password should not work
         """
-        current_password = "Pa55word!"
         new_password = "TimT@tman!"
-        user.validate_password(current_password)
-        modified = s.user_change_password(user=user, new_password=new_password, password=current_password)
+        user.validate_password(password)
+        modified = s.user_change_password(user=user, new_password=new_password, password=password, url_path="some-path")
         assert user == modified == User.objects.get(pk=user.pk)
         # Password should be hashed
         assert modified.password != new_password
@@ -71,36 +72,26 @@ class TestUserChangePassword:
         modified.validate_password(new_password)
         # Should raise error
         with pytest.raises(ValidationError) as exc:
-            modified.validate_password(current_password)
+            modified.validate_password(password)
         assert "password" in exc.value.detail
 
-    def test_password_change_sends_email(
-        self,
-        user: User,
-        mailoutbox: list[EmailMessage],
-        django_capture_on_commit_callbacks,
-    ) -> None:
-        """
-        Check the mail is sent to the correct user
-        contains the correct subject, links, and body
-        """
-        from leasify.common.emails import email_context
+    def test_case_when_resetting_password(self, user: User, password: str, patch_notify_password_change: MagicMock):
+        """In case of resetting, password is not required"""
 
-        current_password = "Pa55word!"
-        new_password = "TimT@tman!"
-        user.validate_password(current_password)
-        with django_capture_on_commit_callbacks(execute=True) as callbacks:
-            s.user_change_password(user=user, new_password=new_password, password=current_password)
-        assert len(callbacks) == 1
-        assert len(mailoutbox) == 1
-        mail = mailoutbox[0]
-        assert mail.subject == "Account password has been changed"
-        assert mail.from_email == email_context.default_from_email
-        assert mail.to == [user.email]
-        assert "password-reset" in mail.body
+        new_password = "NewPassword"
+        modified = s.user_change_password(user=user, new_password=new_password, is_ressetting=True, url_path="some-path")
+        user.refresh_from_db()
+        assert modified == user
 
-    def test_password_changed_without_password(self, user: User):
-        new_password = "Everl@sting!"
-        mod_user = s.user_change_password(user=user, new_password=new_password, is_ressetting=True)
-        assert mod_user == user
-        mod_user.validate_password(new_password)
+        user.validate_password(new_password)
+
+        with pytest.raises(ValidationError, match="password"):
+            user.validate_password(password)
+
+    def test_mail_sending_called_with_correct_args(self, user: User, patch_notify_password_change: MagicMock, password: str):
+        """Patch delay and confirm called with correct args"""
+
+        frontend_path = "path/to/frontend"
+
+        s.user_change_password(user=user, new_password=password, password=password, url_path=frontend_path)
+        patch_notify_password_change.assert_called_once_with(user.pk, frontend_path)
