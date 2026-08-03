7ce46a6b9e3c2c4a743041e34d8b1ece00589d39
munene25 <edmune25@gmail.com>
2026-08-03 19:04:37 +0300

    refactor(tests): remove unused user_create_payload fixture from conftest.py

--- leasify/tests/users/conftest.py
+++ leasify/tests/users/conftest.py
@@ -1,18 +1,4 @@
 import pytest
-from leasify.tests.types import UserCreatePayload
-
-
-@pytest.fixture
-def user_create_payload(fake, password, phone_no) -> UserCreatePayload:
-    return {
-        "first_name": fake.first_name(),
-        "last_name": fake.last_name(),
-        "email": fake.email(),
-        "password": password,
-        "phone_number": phone_no(),
-        "notify": False
-    }
-
 
 @pytest.fixture(params=["+101-999-222-222", "+222-222-222-222", "+256-722-222-222", "+255712345678"])
 def wrong_phone_number(request) -> str:
