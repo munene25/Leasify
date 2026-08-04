784396af0f07211898645a8b828cc3f026d818d7
munene25 <edmune25@gmail.com>
2026-08-04 13:35:38 +0300

    refactor(tests): remove unused UserCreatePayload and UserUpdatePayload types

--- leasify/tests/types.py
+++ leasify/tests/types.py
@@ -6,23 +6,6 @@ if TYPE_CHECKING:
     from leasify.apartments.models import Apartment
     from datetime import date
 
-
-class UserCreatePayload(TypedDict):
-    first_name: str
-    last_name: str
-    email: str
-    password: str
-    phone_number: str
-    notify: bool
-
-
-class UserUpdatePayload(TypedDict, total=False):
-    first_name: str
-    last_name: str
-    bio: str
-    backup_email: str
-    phone_number: str
-
 class TenancyPayload(TypedDict):
     user: "User"
     apartment: "Apartment"
@@ -72,7 +55,6 @@ class IsClient(Protocol):
     @property
     def cookies(self) -> dict[str, Any]: ...
     
-_UserCreatePayload = Unpack[UserCreatePayload]
 
 
 # ----------------------- Factory  -----------------------
