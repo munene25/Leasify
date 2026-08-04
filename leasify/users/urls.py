46b4a94bb89b5e4b2c8b216378617cc6b5d8865c
munene25 <edmune25@gmail.com>
2026-08-04 15:23:51 +0300

    refactor(urls): update path names for clarity and consistency

--- leasify/users/urls.py
+++ leasify/users/urls.py
@@ -5,10 +5,10 @@ app_name = "users"
 
 urlpatterns = [
     path("me", views.MeView.as_view(), name="me"),
-    path("", views.UserListCreateView.as_view(), name="user_list_create"),
-    path("<int:user_id>", views.AdminDetailUpdateDestroyView.as_view(), name="admin_detail"),
-    path("roles/<int:user_id>", views.AdminRoleDetailView.as_view(), name="admin_role_detail"),
-    path("roles", views.AdminRoleListView.as_view(), name="admin_roles_list"),
+    path("", views.UserListCreateView.as_view(), name="list_create"),
+    path("<int:user_id>", views.AdminDetailUpdateDestroyView.as_view(), name="detail_update_destroy"),
+    path("roles/<int:user_id>", views.AdminRoleDetailView.as_view(), name="role_detail"),
+    path("roles", views.AdminRoleListView.as_view(), name="roles_list"),
     path("email-change", views.EmailUpdateView.as_view(), name="email_change" ),
     path("unsubscribe/<str:uidb64>", views.UserUnsubscribeView.as_view(), name="unsubscribe")
 ]
