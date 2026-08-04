bd5509c48d6d1275047c662f850bdec332766409
munene25 <edmune25@gmail.com>
2026-08-04 17:14:21 +0300

    refactor(exceptions): change PasswordError to inherit from ValidationError for better error handling

--- leasify/common/exceptions.py
+++ leasify/common/exceptions.py
@@ -1,4 +1,4 @@
-from rest_framework.exceptions import APIException
+from rest_framework.exceptions import ValidationError, APIException
 from rest_framework import status
 
 
@@ -31,7 +31,7 @@ class RoleAssignmentError(APIException):
     default_code = "roles_exceeded"
 
 
-class PasswordError(APIException):
+class PasswordError(ValidationError):
     """Raised for incorrect passwords"""
 
     status_code = status.HTTP_400_BAD_REQUEST
