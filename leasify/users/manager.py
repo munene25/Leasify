ccb0c78c1e1ce00534446eb963593148209a291e
munene25 <edmune25@gmail.com>
2026-08-07 16:16:24 +0300

    fix(manager): update email normalization logic and superuser create

--- leasify/users/manager.py
+++ leasify/users/manager.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.base_user import BaseUserManager
 
 
+
 class UserManager(BaseUserManager):
     """"Provide superuser creation and custom email normalization"""
 
@@ -8,29 +9,31 @@ class UserManager(BaseUserManager):
     def normalize_email(cls, email: str | None) -> str:
         """Cleaner email normalization. Lowercase email values"""
 
-        if not email:
-            raise ValueError("Email is not none")
+        email = email or ""
+        
         return email.strip().lower()
     
     def create_superuser(self, email: str, password: str | None = None, **extra_fields):
         """Creates a superuser with the given email and password."""
 
-        from leasify.users.services import user_account_create
+        from leasify.users.services import user_create
+        from leasify.users.models import AccountType
 
         extra_fields.setdefault("phone_number", None)
 
-        user = user_account_create(
+        user = user_create(
             email=email,
             password=password,
             first_name=extra_fields['first_name'],
-            last_name=extra_fields['first_name'],
+            last_name=extra_fields['last_name'],
             phone_number=extra_fields["phone_number"],
+            account_type=AccountType.EMAIL,
             notify=False,
+            is_superuser=True,
+            is_staff=True,
+            verified=True,
+
         )
-        user.is_superuser = True
-        user.is_staff = True
-        user.verified = True
-        user.save()
         return user
 
 
