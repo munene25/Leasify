fd4c50a2533ac3766b063cd8a6b146f0fd2417ba
munene25 <edmune25@gmail.com>
2026-08-17 13:14:20 +0300

    fix(urls): correct user role detail path format

--- leasify/users/urls.py
+++ leasify/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from leasify.users import views
 
 app_name = "users"
 
@@ -7,7 +7,7 @@ urlpatterns = [
     path("me", views.MeView.as_view(), name="me"),
     path("", views.UserListCreateView.as_view(), name="list_create"),
     path("<int:user_id>", views.AdminDetailDestroyView.as_view(), name="admin_detail"),
-    path("roles/<int:user_id>", views.AdminRoleDetailView.as_view(), name="role_detail"),
+    path("<int:user_id>/role", views.AdminRoleDetailView.as_view(), name="role_detail"),
     path("roles", views.AdminRoleListView.as_view(), name="roles_list"),
     path("email-change", views.EmailUpdateView.as_view(), name="email_change" ),
     path("unsubscribe/<str:uidb64>", views.UserUnsubscribeView.as_view(), name="unsubscribe")
