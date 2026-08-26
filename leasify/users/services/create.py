30f42fd1ddba3baf37cd19fd9cf6bd4da08fc975
munene25 <edmune25@gmail.com>
2026-08-26 12:07:14 +0300

    chore: improve docstring for user_create

--- leasify/users/services/create.py
+++ leasify/users/services/create.py
@@ -13,7 +13,26 @@ logger = getLogger("users.services.create")
 
 @transaction.atomic
 def user_create(email: str, first_name: str, last_name: str, account_type: AccountType, notify: bool, **kwargs: Any) -> User:
-    """Create user and account based on the account types."""
+    """
+    Create a new user and account based on the provided account type.
+
+    :param email: The email address of the new user. Must be a valid email format.
+    :param first_name: The first name of the new user.
+    :param last_name: The last name of the new user.
+    :param account_type: The type of account to create (e.g., 'customer', 'staff', 'superuser').
+    :param notify: If True, sends a welcome email with unsubscribe and email verification URLs.
+    :param kwargs: Additional keyword arguments passed for account creation.
+        - is_staff: Whether the user is a staff member (bool)
+        - is_active: Whether the user is active (bool)
+        - is_superuser: Whether the user is a superuser (bool)
+        - backup_email: An optional backup email address (str)
+        - phone_number: An optional phone number (str)
+        - unsubscribe_url: URL for unsubscribing from emails (str)
+        - email_verify_url: URL for email verification (str)
+    
+    :return: The created User object.
+    :raises: Various exceptions depending on the account type handler.
+    """
 
     email = User.objects.normalize_email(email)
 
